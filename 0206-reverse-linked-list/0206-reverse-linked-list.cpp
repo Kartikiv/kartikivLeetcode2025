0bcd0463f0a5829222916b2b4bb331dbd4365484
Kartikiv <122755518+Kartikiv@users.noreply.github.com>
2025-10-26 12:10:22 -0700

    Time: 0 ms (100%), Space: 13.3 MB (90.39%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -8,6 +8,12 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+ // This is a simple reversal
+ /*
+ 1. we need a prev pointer to reverse the link 
+ 2. we need a next pointer to preserve the next node location 
+ 3. And finally a current pointer so that we can perform operation like link reversing
+ */
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
