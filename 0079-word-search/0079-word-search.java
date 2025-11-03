b6c0fb5bdf86bd58243a5fd6d24ce05115ba94ae
Kartikiv <122755518+Kartikiv@users.noreply.github.com>
2025-11-03 15:04:54 -0800

    Time: 312 ms (7.79%), Space: 46.7 MB (6.35%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,46 @@
+class Solution {
+    char[][] board;
+    String word;
+    boolean flag = false;
+
+    public boolean exist(char[][] board, String word) {
+        this.board = board;
+        this.word = word;
+        StringBuilder current = new StringBuilder();
+        boolean[][] visited = new boolean[board.length][board[0].length];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                helper(i, j, current, visited,0);
+            }
+        }
+        return flag;
+    }
+
+    public void helper(int i, int j, StringBuilder current, boolean visited[][], int index) {
+        if (flag || i < 0 || j < 0 || i >= board.length || j >= board[0].length) {
+            return;
+        }
+        if (visited[i][j]) {
+            return;
+        }
+          if (board[i][j] != word.charAt(index)) {
+            return;
+        }
+
+        current.append(board[i][j]);
+        if (current.toString().equals(word)) {
+            flag = true;
+            return;
+        }
+        visited[i][j] = true;
+        helper(i + 1, j, current, visited,index + 1);
+        helper(i, j - 1, current, visited, index + 1);
+
+        helper(i - 1, j, current, visited, index + 1);
+        helper(i, j + 1, current, visited, index + 1);
+
+        visited[i][j] = false;
+        current.deleteCharAt(current.length() - 1);
+
+    }
+}
