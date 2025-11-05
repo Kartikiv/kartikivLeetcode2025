0051e81f5a6e9c75e5add0ccfc5a06384a18a1b5
Kartikiv <122755518+Kartikiv@users.noreply.github.com>
2025-11-04 21:58:38 -0800

    Time: 27 ms (95.31%), Space: 146.8 MB (5.16%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -0,0 +1,53 @@
+// One idea is tro implement a priority queue of the length of the window 
+// next we add and remove elements vise versa 
+// this make the implemenmtation effecient 
+// here deletion from the pq is the main issue it O n 
+// so algo is o (n . k) worst case
+// marked for revisit need to learn monotonic queue
+/*  
+1. one way to inmplenet a monotonic stack might be in the window we will add all elements in the increasing order as 
+win 1 : [1, 3 ]
+win 2 : []
+ */
+ class QueueNode{
+    int val; 
+    int index;
+    public QueueNode(int val, int index){
+        this.val = val;
+        this.index = index;
+    }
+    public String toString(){
+        return this.val + " " + this.index;
+    }
+ }
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+    Deque <QueueNode> windowQueue = new LinkedList<>();
+    int ans[] = new int [nums.length - k + 1];    
+    int l =0 ; 
+    int r = 0;
+    for(; r < k ; r++){
+        while(!windowQueue.isEmpty() &&windowQueue.peekLast().val < nums[r]){
+            windowQueue.pollLast();
+        }
+        windowQueue.add(new QueueNode(nums[r], r));
+    }
+    while (r < nums.length) {
+        QueueNode node = windowQueue.peekFirst();
+        ans[l] = node.val;
+        l++;
+        while (!windowQueue.isEmpty() && node.index < l){
+            windowQueue.pollFirst();
+            node = windowQueue.peekFirst();
+        }
+         while(!windowQueue.isEmpty() &&windowQueue.peekLast().val < nums[r]){
+            windowQueue.pollLast();
+        }
+        windowQueue.add(new QueueNode(nums[r], r));
+
+        r++;
+        }
+        ans[l] = windowQueue.pollFirst().val;
+ return ans ;   
+ }
+}
