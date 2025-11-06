d453f93367315b7721a8d007627377e6f82285d0
Kartikiv <122755518+Kartikiv@users.noreply.github.com>
2025-11-06 10:58:08 -0800

    Time: 4 ms (48.82%), Space: 51.2 MB (30.53%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,11 +1,10 @@
 class Solution {
-    boolean visited[][] ;
     public int numIslands(char[][] grid) {
-        this.visited = new boolean[grid.length][ grid[0].length ];
+        
         int count = 0;
         for(int i = 0; i < grid.length ; i++){
             for(int j = 0 ; j < grid[0].length ; j++){
-                if(!visited[i][j] && grid[i][j] == '1'){
+                if(grid[i][j] == '1'){
                     bfsGraph(grid, i, j);
                     count++;    
                 }
