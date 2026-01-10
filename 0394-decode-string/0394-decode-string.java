35bd18ce782b9a31eeafbc9b98dfe43385126ce4
Kartikiv <122755518+Kartikiv@users.noreply.github.com>
2026-01-10 15:26:50 -0800

    Time: 2 ms (33.47%), Space: 42.8 MB (74.76%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,14 +1,12 @@
 class Solution {
     public String decodeString(String s) {
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == ']') {
-                List<Character> decodedString = new ArrayList<>();
-                // get the encoded string
-                while (stack.peek() != '[') {
-                    decodedString.add(stack.pop());
+        Stack <Character> stack = new Stack<>();
+        for (char c : s.toCharArray()){
+            if( c == ']'){
+                List<Character> charList = new ArrayList<>();
+                while (!stack.isEmpty() && stack.peek() != '['){
+                    charList.add(stack.pop());
                 }
-                // pop [ from the stack
                 stack.pop();
                 int base = 1;
                 int k = 0;
@@ -17,21 +15,23 @@ class Solution {
                     k = k + (stack.pop() - '0') * base;
                     base *= 10;
                 }
-                // decode k[decodedString], by pushing decodedString k times into stack
+
                 while (k != 0) {
-                    for (int j = decodedString.size() - 1; j >= 0; j--) {
-                        stack.push(decodedString.get(j));
+                    for (int j = charList.size() - 1; j >= 0; j--) {
+                        stack.push(charList.get(j));
                     }
                     k--;
                 }
-            }
-            // push the current character to stack
-            else {
-                stack.push(s.charAt(i));
-            }
-        }      
-        // get the result from stack
-        char[] result = new char[stack.size()];
+             }
+             else{
+                    stack.push(c);
+                }
+        }
+
+
+
+
+ char[] result = new char[stack.size()];
         for (int i = result.length - 1; i >= 0; i--) {
             result[i] = stack.pop();
         }
