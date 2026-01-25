4e2861c7818052cb5b5947b8a7298e799ca2bb0b
Kartikiv <122755518+Kartikiv@users.noreply.github.com>
2026-01-24 18:34:12 -0800

    Time: 0 ms (100%), Space: 42 MB (63.76%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,18 @@
 class Solution {
     public int climbStairs(int n) {
-        int [] dp = new int [n + 1];
+        int[] dp = new int[n + 1];
 
         dp[0] = 1;
         dp[1] = 1;
 
-        for(int i = 2; i < n + 1; i++){
+        for (int i = 2; i < n + 1; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
-    return dp[n]; 
+        return dp[n];
+    }
 }
-}
+// Clasic Dp 
+// the way we can reach the nth step is by either reaching n-1 th step or n - 2 step 
+// Therefore the total no of ways of reach is the sum of the ways of reach n - 1 and n - 2
+// Simple idea
+// Direct tablulation 
