092ce2b50c7dd8f40322b87ae1def008f99092a5
Kartikiv <122755518+Kartikiv@users.noreply.github.com>
2026-01-27 21:18:17 -0800

    Time: 1 ms (99.59%), Space: 77.2 MB (58.98%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,4 +1,18 @@
+/**
+ * Finds the contiguous subarray with the maximum sum.
+ * 
+ * This method implements a variant of Kadane's algorithm to find the maximum sum
+ * of a contiguous subarray within the given array of integers.
+ * 
+ * @param nums an array of integers which may contain positive, negative, or zero values
+ * @return the maximum sum of any contiguous subarray; returns Integer.MIN_VALUE if the array is empty
+ * 
+ * @example
+ * Input: nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+ * Output: 6 (subarray [4, -1, 2, 1])
+ */
 class Solution {
+
     public int maxSubArray(int[] nums) {
         int max = Integer.MIN_VALUE;
         int sum = 0;
