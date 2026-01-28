411f5877194b6ed8de32f7d15677c6524b99edcb
Kartikiv <122755518+Kartikiv@users.noreply.github.com>
2026-01-28 10:22:33 -0800

    Time: 24 ms (78.1%), Space: 48.8 MB (56.3%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,7 +1,23 @@
 import java.util.HashMap;
 
 class Solution {
-    public int subarraySum(int[] nums, int k) {
+
+    /**
+     * Finds the number of contiguous subarrays whose elements sum to a given target
+     * value.
+     * 
+     * Uses a prefix sum approach with a HashMap to efficiently count subarrays.
+     * For each element, checks if there exists a previous prefix sum such that
+     * (current prefix sum - previous prefix sum) equals the target value k.
+     * 
+     * Time Complexity: O(n) where n is the length of the input array
+     * Space Complexity: O(n) for the HashMap storing prefix sums
+     * 
+     * @param nums the input array of integers
+     * @param k    the target sum to find in subarrays
+     * @return the count of contiguous subarrays that sum to k
+     */
+    public int subarraySum(int[] nums, int k) { 
         HashMap<Integer, Integer> preFixMap = new HashMap<>();
         int sum = 0;
         int ans = 0;
@@ -9,12 +25,10 @@ class Solution {
         preFixMap.put(0, 1);
         for (int num : nums) {
             sum += num;
-            ans += preFixMap.getOrDefault(sum - k, 0);  
+            ans += preFixMap.getOrDefault(sum - k, 0);
             preFixMap.put(sum, preFixMap.getOrDefault(sum, 0) + 1);
 
-
-
-            
         }
-    return ans; }
+        return ans;
+    }
 }
