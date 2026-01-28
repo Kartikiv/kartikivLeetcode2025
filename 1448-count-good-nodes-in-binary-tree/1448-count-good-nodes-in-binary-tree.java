e8741df8881ae6eef51140816e9ccf6bed25bde5
Kartikiv <122755518+Kartikiv@users.noreply.github.com>
2026-01-27 21:10:52 -0800

    Time: 2 ms (92.34%), Space: 57.1 MB (38.74%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -1,33 +1,45 @@
 /**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
+ * Solution for counting good nodes in a binary tree.
+ * A node is considered "good" if the value of the node is greater than or equal
+ * to
+ * the maximum value seen on the path from root to that node.
  */
 class Solution {
-    int ans = 0 ;
-    int rootVal;
+    /**
+     * Counts the number of good nodes in a binary tree.
+     * A good node is one where its value is >= the maximum value on the path from
+     * root.
+     *
+     * @param root the root node of the binary tree
+     * @return the total count of good nodes in the tree
+     * @time O(n) where n is the number of nodes in the tree
+     * @space O(h) where h is the height of the tree (recursion stack)
+     */
     public int goodNodes(TreeNode root) {
-       rootVal = root.val;
-       dfs(root, Integer.MIN_VALUE);
-       return ans;
+        return dfs(root, Integer.MIN_VALUE);
     }
-   public void dfs (TreeNode root, int maxSofar){
-    if(root == null) return; 
-    if(root.val >= maxSofar){
-        ans++;
-        maxSofar = root.val;
+
+    /**
+     * Helper method that performs depth-first search to count good nodes.
+     *
+     * @param node     the current node being processed
+     * @param maxSoFar the maximum value seen from root to the parent of current
+     *                 node
+     * @return the count of good nodes in the subtree rooted at the current node
+     */
+    private int dfs(TreeNode node, int maxSoFar) {
+
+        if (node == null)
+            return 0;
+
+        int count = 0;
+        if (node.val >= maxSoFar) {
+            count = 1;
+            maxSoFar = node.val;
+        }
+
+        return count
+                + dfs(node.left, maxSoFar)
+                + dfs(node.right, maxSoFar);
     }
-    dfs(root.right, maxSofar);
-    dfs(root.left, maxSofar);
-}
 }
