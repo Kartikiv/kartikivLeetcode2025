e53c49a43c826178b540994cc1dcc4717258d789
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-02-13 13:45:20 -0600

    Time: 1 ms (99.68%), Space: 62.1 MB (70.03%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,19 +1,31 @@
 class Solution {
+    /*
+     * ==================== PROBLEM FRAMEWORK ====================
+     * 1) Pattern:Sliding Window
+     * 2) Inputs/Guarantees: given a target and an array
+     * 3) Goal: length of minimum subarray with sum == target
+     * 4) Invariant: take a window and keep add till the sum is less than equal to
+     * target
+     * if sum is greater than target move the window right
+     * 5) Decision Rules: if(sum > target ) left ++;
+     * 6) Why correct:
+     * 7) Complexity: Time O(), Space O()
+     * ============================================================
+     */
     public int minSubArrayLen(int target, int[] nums) {
         int i = 0;
+        int j = 0;
         int sum = 0;
-        int minLen = Integer.MAX_VALUE;
-
-        for (int j = 0; j < nums.length; j++) {
+        int minWindowLen = Integer.MAX_VALUE;
+        while (j < nums.length) {
             sum += nums[j];
-
             while (sum >= target) {
-                minLen = Math.min(minLen, j - i + 1);
+                minWindowLen = Math.min(minWindowLen, j - i + 1);
                 sum -= nums[i];
                 i++;
             }
+            j++;
         }
-
-        return minLen == Integer.MAX_VALUE ? 0 : minLen;
+        return minWindowLen == Integer.MAX_VALUE ? 0 : minWindowLen;
     }
 }
