12ad6760eecc616bc0e1ede2616481fe285611f1
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-02-13 15:09:55 -0600

    Time: 21 ms (36.38%), Space: 46.9 MB (19.84%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,48 +1,52 @@
+
 import java.util.*;
 
 class Solution {
-    int[] charMap = new int[26];
-
+    /*
+     * ==================== PROBLEM FRAMEWORK ====================
+     * 1) Pattern: SLiding window
+     * 2) Inputs/Guarantees:
+     * 3) Goal:
+     * 4) Invariant:
+     * 5) Decision Rules:
+     * 6) Why correct:
+     * 7) Complexity: Time O(), Space O()
+     * ============================================================
+     */
     public boolean checkInclusion(String s1, String s2) {
-        int m = s1.length(), n = s2.length();
-        if (m > n) return false;
+        if(s1.length() > s2.length()) return false;
+        HashMap<Character, Integer> s1HashMap = new HashMap<>();
+        HashMap<Character, Integer> s2HashMap = new HashMap<>();
+        char[] s1Arr = s1.toCharArray();
+        char[] s2Arr = s2.toCharArray();
 
-        // reset charMap in case the same Solution object is reused
-        Arrays.fill(charMap, 0);
+        for (char c : s1Arr) {
+            s1HashMap.put(c, s1HashMap.getOrDefault(c, 0) + 1);
 
-        // charMap = freq(s1)
-        for (char c : s1.toCharArray()) {
-            charMap[c - 'a']++;
         }
-
-        // subtract first window of size m from s2
-        for (int k = 0; k < m; k++) {
-            charMap[s2.charAt(k) - 'a']--;
+        int i = 0;
+        int j = 0;
+        while (j < s1Arr.length) {
+            s2HashMap.put(s2Arr[j], s2HashMap.getOrDefault(s2Arr[j], 0) + 1);
+            j++;
         }
+        while (j < s2Arr.length) {
+            if (s1HashMap.equals(s2HashMap))
+                return true;
+            // remove i
+            int freq = s2HashMap.getOrDefault(s2Arr[i], 0) - 1;
+            if (freq == 0) {
+                s2HashMap.remove(s2Arr[i]);
+            } else {
+                s2HashMap.put(s2Arr[i], freq);
+            }
 
-        if (isValidWindow()) return true;
-
-        int i = 0;          // left of window
-        int j = m;          // next index to enter the window
-        while (j < n) {
-            // char leaving window at i
-            charMap[s2.charAt(i) - 'a']++;
+            // add j
+            s2HashMap.put(s2Arr[j], s2HashMap.getOrDefault(s2Arr[j], 0) + 1);
             i++;
-
-            // char entering window at j
-            charMap[s2.charAt(j) - 'a']--;
             j++;
 
-            if (isValidWindow()) return true;
         }
-
-        return false;
-    }
-
-    boolean isValidWindow() {
-        for (int x : charMap) {
-            if (x != 0) return false;
-        }
-        return true;
+        return s1HashMap.equals(s2HashMap);
     }
 }
