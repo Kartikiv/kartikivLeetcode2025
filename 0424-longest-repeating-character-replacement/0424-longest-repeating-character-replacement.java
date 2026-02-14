f7919a3f00db50871109969a1f70363b021b1a47
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-02-13 19:14:33 -0600

    Time: 6 ms (98.7%), Space: 46.6 MB (30.19%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,7 +1,19 @@
 
 class Solution {
     int k;
-
+    /*
+     * ==================== PROBLEM FRAMEWORK ====================
+     * 1) Pattern: Sliding Window 
+     * 2) Inputs/Guarantees: String s and k 
+     * 3) Goal: return the maximum window length after the character
+     * replacement
+     * 4) Invariant: we maintain a window which isvalid and in the valid window
+     * the condition is j - i + 1 - maxFreq <= k.
+     * 5) Decision Rules: if window is invalid move the i pointer towards the right.
+     * 6) Why correct: we are always maintiaining a window where all the replacements are possible.
+     * 7) Complexity: Time O(n), Space O(1).
+     * ============================================================
+     */
     public int characterReplacement(String s, int k) {
         this.k = k;
         int[] charMap = new int[26];
