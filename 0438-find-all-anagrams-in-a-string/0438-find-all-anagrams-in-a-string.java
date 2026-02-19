99fe8c741ab577517a2240ccf18924c480785bd7
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-02-19 14:53:12 -0800

    Time: 9 ms (93.34%), Space: 46.9 MB (71.88%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,37 +1,28 @@
-// create a wind of length p and move the window one character at a time
-// if the frequency is the same add the begin index and move on.
 import java.util.*;
 
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        if(s.length() < p.length()) return new ArrayList<>();
-        List<Integer> ans = new ArrayList<>();
+        if(p.length() > s.length()) return new ArrayList<>();
+        List<Integer> answer = new ArrayList<>();
         int[] windowFreq = new int[26];
-        int left = 0;
-        int j = p.length() - 1;
-        for(int i = 0 ; i < p.length(); i++){
+        for (int i = 0; i < p.length(); i++) {
             windowFreq[p.charAt(i) - 'a']++;
         }
-        for(int i = 0 ; i < p.length(); i++){
+        for (int i = 0; i < p.length(); i++) {
             windowFreq[s.charAt(i) - 'a']--;
         }
-
-        while (j < s.length() - 1) {
-            if(isValidWindow(windowFreq)){
-                ans.add(left);
-            }
-            windowFreq[s.charAt(left) - 'a']++;
-            left++;
-            j++;
+        if(isValid(windowFreq)) answer.add(0);
+        for (int i = 0, j = p.length(); j < s.length(); j++) {
             windowFreq[s.charAt(j) - 'a']--;
+            windowFreq[s.charAt(i) - 'a']++;
+         
+            i++;
+            if (isValid(windowFreq)) answer.add(i);
         }
-        if(isValidWindow(windowFreq)){
-            ans.add(left);
-        }
-  return ans;
-  }
+    return answer; 
+}
 
-    boolean isValidWindow(int[] windowFreq) {
+    boolean isValid(int[] windowFreq) {
         for (int num : windowFreq) {
             if (num != 0)
                 return false;
