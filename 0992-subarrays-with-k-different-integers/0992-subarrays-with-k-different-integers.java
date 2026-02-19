739626a075ae1404a9f8450bfa22710dd848f68e
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-02-19 14:30:42 -0800

    Time: 39 ms (86.99%), Space: 49.1 MB (84.84%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -1,48 +1,29 @@
+
 import java.util.HashMap;
-import java.util.Map;
 
 class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
-        Map<Integer, Integer> freq = new HashMap<>();
 
-        int left = 0;
-        int right = 0;
+    return  slidingWindowAtMostk(nums, k) - slidingWindowAtMostk(nums, k - 1); 
+}
 
-        int distinctRemaining = k; // how many new distincts we can still add
-        int duplicatePrefix = 0;  // how many times we can move left over duplicates
-        int ans = 0;
-
-        while (right < nums.length) {
-
-            // 1) expand window by adding nums[right]
-            int x = nums[right];
-            freq.put(x, freq.getOrDefault(x, 0) + 1);
-            if (freq.get(x) == 1) distinctRemaining--; // new distinct added
-            right++;
-
-            // 2) too many distinct -> shrink once until we’re back to <= k
-            if (distinctRemaining < 0) {
-                int y = nums[left];
-                freq.put(y, freq.get(y) - 1);
-                if (freq.get(y) == 0) freq.remove(y);
-                left++;
-
-                distinctRemaining++;   // we removed one distinct type from the window
-                duplicatePrefix = 0;   // reset because left boundary changed “for real”
-            }
-
-            // 3) exactly k distinct -> compress duplicates on the left and count
-            if (distinctRemaining == 0) {
-                while (freq.get(nums[left]) > 1) {
-                    int y = nums[left];
-                    freq.put(y, freq.get(y) - 1);
-                    left++;
-                    duplicatePrefix++;
+    int slidingWindowAtMostk(int[] nums, int k) {
+        HashMap<Integer, Integer> set = new HashMap<>();
+        int maxLen = 0;
+        for (int i = 0, j = 0; j < nums.length; j++) {
+            set.put(nums[j], set.getOrDefault(nums[j], 0) + 1);
+            while (set.size() > k) {
+                int freq = set.getOrDefault(nums[i], 0);
+                freq = freq - 1;
+                if (freq == 0) {
+                    set.remove(nums[i]);
+                } else {
+                    set.put(nums[i], freq);
                 }
-                ans += (duplicatePrefix + 1);
+                i++;
             }
+            maxLen += j - i + 1;
         }
-
-        return ans;
+        return maxLen;
     }
 }
