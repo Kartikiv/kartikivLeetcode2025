375106ab72850e12069b6874b1879a9d37a77302
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-02-21 10:01:11 -0800

    Time: 7 ms (9.26%), Space: 48.5 MB (7.7%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+class Solution {
+      public int pivotIndex(int[] nums) {
+        HashMap<Integer, Integer> maps = new HashMap<>();
+        int sum = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            maps.put(i, sum);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if(maps.get(i) == maps.get(nums.length - 1) - maps.getOrDefault(i - 1, 0)){
+                return i ;
+            }
+        }
+
+
+   return -1; }
+}
