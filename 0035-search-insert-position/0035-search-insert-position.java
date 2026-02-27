741090720c1d292356791b36e6113d4ea9237b34
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-02-27 11:18:40 -0800

    Time: 0 ms (100%), Space: 44.6 MB (83.94%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,23 +1,19 @@
+// an insert position is the place where the upper bound number is ?? 
+// is this true ?? 
+// if not why ?? 
+
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        
-    return floor(nums, target) + 1;
-    }
+     int left = 0; 
+     int right = nums.length - 1; 
 
-    int floor(int[] arr, int target) {
-    int lo = 0, hi = arr.length - 1;
-    int ans = -1;
-
-    while (lo <= hi) {
-        int mid = lo + (hi - lo) / 2;
-
-        if (arr[mid] < target) {
-            ans = mid;   // candidate
-            lo = mid + 1;     // try for a bigger one
-        } else {
-            hi = mid - 1;
+     while (left < right){
+        int mid = left + (right - left) / 2 ;
+        if(nums[mid] >= target){
+            right = mid;
+        }else{
+            left = mid + 1;
         }
-    }
-    return ans;
-}
+     }   
+    return nums[right] < target ? nums.length : right ;}
 }
