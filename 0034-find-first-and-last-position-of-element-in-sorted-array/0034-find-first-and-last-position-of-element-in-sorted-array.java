706badc45312d43a53e1617e73d7117df7f6ba53
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-02-28 15:20:04 -0800

    Time: 0 ms (100%), Space: 48.4 MB (28.85%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,35 +1,43 @@
+
+import java.lang.annotation.Target;
+
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        if (nums.length == 0) return new int[]{-1, -1};
+        if(nums.length == 0) return new int [] {-1,-1};
+        int left = lowerBoundSearch(nums, target); 
+        int right = upperBoundSearch(nums, target);
+        if(nums[left] != target) return new int[]{-1,-1};
+        int [] ans = new int [] {left , right};
+   return ans ;}
 
-        int first = lowerBound(nums, target);   // first >= target
-        if (first == nums.length || nums[first] != target) {
-            return new int[]{-1, -1};
+    int lowerBoundSearch(int[] nums, int target) {
+        int left = 0; 
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) /2;
+            if (nums[mid] >= target) {
+                right = mid;
+            }else{
+                left = mid + 1;
+            }
         }
 
-        int upper = upperBound(nums, target);   // first > target
-        return new int[]{first, upper - 1};
-    }
+    return right; 
+}
 
-    // first index where nums[idx] >= target
-    private int lowerBound(int[] nums, int target) {
-        int i = 0, j = nums.length; // [i, j)
-        while (i < j) {
-            int mid = i + (j - i) / 2;
-            if (nums[mid] >= target) j = mid;
-            else i = mid + 1;
-        }
-        return i;
-    }
+    int upperBoundSearch(int[] nums, int target) {
+        int left = 0; 
+        int right = nums.length - 1;
 
-    // first index where nums[idx] > target
-    private int upperBound(int[] nums, int target) {
-        int i = 0, j = nums.length; // [i, j)
-        while (i < j) {
-            int mid = i + (j - i) / 2;
-            if (nums[mid] > target) j = mid;
-            else i = mid + 1;
+        while (left < right) {
+            int mid = left + (right - left + 1) / 2 ;
+            if(nums[mid] <= target){
+                left = mid;
+            }else{
+                right = mid - 1;
+            }
         }
-        return i;
-    }
+
+    return left; 
+}
 }
