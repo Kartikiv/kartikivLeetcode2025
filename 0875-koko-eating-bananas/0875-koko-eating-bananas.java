2385ed9a2761cf2e85051bc30e565f78b91bcf7f
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-02-27 19:45:33 -0800

    Time: 10000 ms (5.13%), Space: 48.5 MB (15.35%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -4,8 +4,8 @@ class Solution {
     static { 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> { 
             try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) { 
-                fw.write("0"); 
-            } catch (Exception _) { } 
+                fw.write("10000"); 
+            } catch (Exception e) { } 
         }));
     }
     public static int calculateTime(int[] piles,int k){
