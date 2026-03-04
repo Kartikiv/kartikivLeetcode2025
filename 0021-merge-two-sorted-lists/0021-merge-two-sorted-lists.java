b713b09d421e79b14b1b1174757778e96a5ca376
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-03-03 20:23:26 -0800

    Time: 2 ms (2.44%), Space: 44.4 MB (37.41%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -1,3 +1,6 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -10,24 +13,22 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b) -> Integer.      compare(a.val, b.val));
-        ListNode ans = new ListNode(-1);
-        ListNode dummy = ans;
+        if(list1 == null && list2 == null) return null;
         if(list1 == null ) return list2;
-        if(list2 == null)  return list1;
+        if(list2 == null) return list1;
+        ListNode dummy = new ListNode(-1);
+        ListNode ans = dummy;
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b) -> a.val - b.val);
         pq.add(list1);
         pq.add(list2);
+        while(!pq.isEmpty()){
+            ListNode l1 = pq.poll();
+            ans.next = new ListNode(l1.val);
+            ans = ans.next;
+            if(l1.next != null)  pq.add(l1.next);
 
-        while (!pq.isEmpty()) {
-            ListNode node = pq.poll();
-            dummy.next = node;
-            dummy = dummy.next;
-            if(node.next != null){
-                node = node.next;
-                pq.add(node);
-            }
         }
 
-    return ans.next; 
+        return dummy.next;
     }
 }
