d619b66607997eccac94957e787b7b4a1f1592fd
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-03-06 09:30:16 -0800

    Time: 1 ms (79.42%), Space: 43.8 MB (61.48%) - LeetHub

---
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        if(root == null) return  new ArrayList<>();
+        // level order travesal and every alternative level gets flidpped
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        queue.add(root);
+        boolean isFlipped = false;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+               level.add(node.val);
+               if(node.left != null)  queue.add(node.left);
+               if(node.right != null) queue.add(node.right);
+
+            }
+            if (isFlipped){
+                ans.add(level.reversed());
+            }else {
+                ans.add(level);
+            }
+            isFlipped = !isFlipped;
+        }
+   return ans;  }
+}
