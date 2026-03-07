51ffc6842b52d798fd19aa53fad1e17cbed169a8
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-03-07 14:49:40 -0800

    Time: 5 ms (30.35%), Space: 51.3 MB (95.06%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,53 +1,45 @@
+import java.util.LinkedList;
+import java.util.Queue;
+class Node {
+    int i ;
+    int j ;
+    public Node(int i , int j){
+        this.i = i;
+        this.j = j;
+    }
+}
 class Solution {
     public int numIslands(char[][] grid) {
-        
         int count = 0;
-        for(int i = 0; i < grid.length ; i++){
-            for(int j = 0 ; j < grid[0].length ; j++){
-                if(grid[i][j] == '1'){
-                    bfsGraph(grid, i, j);
-                    count++;    
+    for(int i = 0 ; i < grid.length ; i++){
+        for (int j = 0 ; j < grid[0].length ; j++){
+            if (grid[i][j] == '1'){
+                bfs(grid, i ,j);
+                count++;
+            }
+        }
+    }
+   return count;
+    }
+    void bfs(char[][] grid, int i , int j){
+        int [][] directions = new int [][] {{-1,0}, {0,-1}, {0,1}, {1,0}};
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(i,j));
+        grid[i][j] = 0;
+        while (!queue.isEmpty()){
+            Node node = queue.poll();
+            for (int [] direction : directions){
+                int newi = node.i + direction[0];
+                int newj = node.j + direction[1];
+                if(newi >= 0 && newi < grid.length 
+                    && newj >= 0 && newj <grid[0].length 
+                    && grid[newi][newj] == '1'){
+                    queue.add(new Node(newi, newj));
+                    grid[newi][newj] = 0 ;
                 }
+
             }
         }
-        
-                                
-    return count; 
+
     }
-    public void bfsGraph(char [][] grid, int i , int j){
-        Queue<int []> queue = new LinkedList<>();
-        int [] node  = new int[] {i, j};
-        queue.add(node);
-        grid[i][j] = '0';
-        while (!queue.isEmpty()) {
-            node  = queue.poll();
-            i = node[0];
-            j = node[1];
-
-            if(i + 1 < grid.length && grid[i + 1][j] == '1' ){
-                queue.add(new int[] {i + 1, j});
-                 grid[i + 1][j] = '0';
-                
-            }
-            if(i - 1 >= 0  && grid[i - 1][j] == '1' ){
-                queue.add(new int[] {i - 1, j});
-                 grid[i - 1][j] = '0';
-                
-            }
-            if(j + 1 < grid[0].length && grid[i][j + 1] == '1' ){
-                queue.add(new int[] {i, j + 1 });
-                 grid[i][j + 1] = '0';
-                
-            }
-            if( j - 1 >= 0  && grid[i][j - 1] == '1' ){
-                queue.add(new int[] {i, j - 1 });
-                 grid[i][j - 1] = '0';
-                
-            }
-
-
-            
-        }
-    }
-
 }
