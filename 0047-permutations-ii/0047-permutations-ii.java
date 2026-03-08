492777495ba5fa4e85a5f91a283a1fdd016a9723
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-03-08 06:33:59 -0700

    Time: 3 ms (89.97%), Space: 46.5 MB (88.83%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,28 +1,34 @@
 import java.util.*;
 
 class Solution {
-    Set<List<Integer>> ans = new HashSet<>();
+    List<List<Integer>> ans = new ArrayList<>();
+
     public List<List<Integer>> permuteUnique(int[] nums) {
-    List<Integer> list = new ArrayList<>();
-    boolean used[] = new boolean[nums.length];
-    dfs(nums,used,list);
-
-
-    return  new ArrayList(ans);
+        Arrays.sort(nums);
+        boolean[] used = new boolean[nums.length];
+        dfs(nums, used, new ArrayList<>());
+        return ans;
     }
-     void dfs(int [] nums , boolean [] used, List<Integer> list){
-        if(list.size() == nums.length){
+
+    void dfs(int[] nums, boolean[] used, List<Integer> list) {
+        if (list.size() == nums.length) {
             ans.add(new ArrayList<>(list));
             return;
         }
-        for(int i = 0 ; i < nums.length; i++){
-            if(!used[i]){
-                list.add(nums[i]);
-                used[i] = true;
-                dfs(nums, used,list);
-                list.remove(list.size() - 1);
-                used[i] = false;
-            }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) continue;
+
+            // skip duplicates
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
+
+            used[i] = true;
+            list.add(nums[i]);
+
+            dfs(nums, used, list);
+
+            list.remove(list.size() - 1);
+            used[i] = false;
         }
     }
 }
