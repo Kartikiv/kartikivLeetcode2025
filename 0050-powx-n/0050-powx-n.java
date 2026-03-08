bf74272fb471453993f057da5b3279a0ea3eafc4
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-03-08 15:20:41 -0700

    Time: 0 ms (100%), Space: 48.3 MB (32.7%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,25 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long power = n; 
+        if(x == 0 && power == 0) return 1; 
+        if(x == 0 ) return 0; 
+        if(power == 0 ) return 1;
+        if(n < 0){
+            x = 1/x;
+            power = -1 * power;
+        }
+        double result = 1; 
+        while(power > 0){
+            if(power % 2 == 1){
+                result *= x;
+                power = power - 1;
+            }
+            x *= x;
+            power = power / 2;
+
+        }
+       
+
+    return result; 
+    }
+}
