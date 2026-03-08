3f413cea18e26fb802d72bd6a5c1a30bab56572c
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-03-08 07:52:30 -0700

    Time: 21 ms (14.29%), Space: 47.6 MB (5.05%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -3,22 +3,26 @@ import java.util.Map;
 
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-    StringBuilder transformS = new StringBuilder();
-    StringBuilder transformT = new StringBuilder();
-    Map<Character, Integer> map = new HashMap<>();
+        StringBuilder transformS = new StringBuilder();
+        StringBuilder transformT = new StringBuilder();
+        Map<Character, Integer> map = new HashMap<>();
+
         for (int i = 0; i < s.length(); i++) {
-            if(!map.containsKey(s.charAt(i))){
+            if (!map.containsKey(s.charAt(i))) {
                 map.put(s.charAt(i), i);
             }
             transformS.append(map.get(s.charAt(i))).append(":");
         }
+
         map.clear();
+
         for (int i = 0; i < t.length(); i++) {
-            if(!map.containsKey(t.charAt(i))){
+            if (!map.containsKey(t.charAt(i))) {
                 map.put(t.charAt(i), i);
             }
             transformT.append(map.get(t.charAt(i))).append(":");
         }
-    return transformS.compareTo(transformT) == 0;
+
+        return transformS.toString().equals(transformT.toString());
     }
 }
