a52c0a2c675ab30eff08b3d59b6f1022abd1fac6
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-03-12 18:47:44 -0700

    Time: 0 ms (100%), Space: 45 MB (64.07%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -14,11 +14,10 @@
  * }
  */
 class Solution {
-    public boolean hasPathSum(TreeNode root, int sum) {
-        if (root == null) return false;
-
-        sum -= root.val;
-        if ((root.left == null) && (root.right == null)) return (sum == 0);
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root == null) return false; 
+        int sum = targetSum - root.val;
+        if(sum == 0 && root.left == null && root.right == null) return true; 
         return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
     }
 }
