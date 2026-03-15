e0a6d5f033e5575447919b2b5c7ad0b6485cf087
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-03-15 16:15:45 -0700

    Time: 19 ms (19.42%), Space: 49.9 MB (45.64%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -2,10 +2,21 @@ class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String, List<String>> map = new HashMap<>(); 
         for (String str : strs){
-            char [] charArr= str.toCharArray(); 
-            Arrays.sort(charArr);
-            if(!map.containsKey(new String(charArr))) map.put(new String(charArr), new ArrayList<>());
-            map.get(new String(charArr)).add(str);
+            String key = getSignature(str);
+            if(!map.containsKey(key)) map.put(key, new ArrayList<>());
+            map.get(key).add(str);
         }
     return map.values().stream().toList();}
+    
+    String getSignature(String key){
+        StringBuilder sb = new StringBuilder();
+        int [] charFreq = new int [26]; 
+        for(char c : key.toCharArray()){
+            charFreq[c - 'a']++;
+        }
+        for(int i : charFreq){
+            sb.append(i).append("#");
+        }
+    return sb.toString(); 
+    }
 }
