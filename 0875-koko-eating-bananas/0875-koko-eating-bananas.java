173cf98bf49d0e63b560f15290c683b3e5e85e77
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-03-16 15:41:03 -0700

    Time: 10 ms (59.46%), Space: 47.8 MB (69.77%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,38 +1,23 @@
-import static java.lang.Math.max;
-
 class Solution {
-   
-    public static int calculateTime(int[] piles,int k){
-        int result = 0;
-        for (int i=0;i<piles.length;i++){
-
-            result += Math.ceil((double) piles[i] / k);
-        }
-        return result;
-    }
-    public static int getMax(int[] piles){
-        int result = 1;
-        for (int i = 0; i < piles.length; i++) {
-            result = max(result,piles[i]);
-        }
-        return result;
-    }
-    public static int minEatingSpeed(int[] piles, int h) {
-        int result =h; //8
-        int maxPile = getMax(piles);
-        int left =1;
-
-        int right = maxPile;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            int hours  = calculateTime(piles,mid);
-            if (hours > h) {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left = 1 ;
+        int right = Arrays.stream(piles).max().getAsInt();
+        while (left < right) {
+            int mid = left + (right - left) / 2 ; 
+            // search left  
+            if(canEat(piles, mid, h)){
+                right = mid;
+            }else{
                 left = mid + 1;
-            } else {
-                result = mid;
-                right = mid - 1;
             }
         }
-        return result;
+    return right; 
+    }
+    boolean canEat(int [] piles ,int currentSpeed, int h){
+        int timeConsumed = 0 ;
+        for (int pile : piles){
+            timeConsumed += Math.ceilDiv(pile, currentSpeed);
+        }
+        return timeConsumed <= h; 
     }
 }
