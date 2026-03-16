7125c3e9f150e9ce012a89a26b49f8d800f12cd5
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-03-16 13:59:18 -0700

    Time: 0 ms (100%), Space: 43.7 MB (78.62%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,36 +1,30 @@
+
+import java.sql.RowId;
+
 class Solution {
     public int search(int[] nums, int target) {
-        int left = searchPivot(nums);
-        int right = nums.length - 1;
-        int ans =binarySearch(nums, left, right, target);
-    return ans == -1 ? binarySearch(nums, 0, left - 1, target): ans; 
-    }
-    public int binarySearch(int [] nums, int left , int right, int target){
-        while(left <= right){
-            int mid = left + (right - left) / 2;
-            if(nums[mid] == target){
-                return mid;
-            } 
-            if(nums[mid] < target){
-                left = mid + 1;
+       int low = 0; 
+       int high = nums.length - 1; 
+       while (low <= high) {
+        int mid = low + (high - low) / 2; 
+        if(nums[mid] == target){
+            return mid; 
+        }
+        // check if left half is sorted and target is in the left half
+        else if(nums[mid] >= nums[low]){
+            if(target >= nums[low] && target < nums[mid]){
+                high = mid - 1;
             }else{
-                right = mid - 1;
+                low = mid + 1; 
+            }
+            // right half is sorted
+        }else{
+            if(target <= nums[high] && target > nums[mid]){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
             }
         }
-    return -1; 
-    }
-    public int searchPivot(int[] nums) {
-        int left = 0 ; 
-        int right = nums.length - 1; 
-        while(left < right){
-            int mid = left + (right - left ) / 2;
-            if(nums[mid] > nums[right]){
-                left = mid + 1;
-            }
-            else{
-                right = mid;
-            }
-        }
-    return left; 
-    }
+       }
+    return  -1; }
 }
