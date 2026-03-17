9b42d5cf9ca62e2d47c495afe39cff349086ab83
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-03-17 15:44:36 -0700

    Time: 5 ms (86.77%), Space: 45.2 MB (86.04%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,26 +1,20 @@
 import java.util.*;
-
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int i = 0;
-        int j = 0;
-        int maxWindow = 0;
+        // expand  the window till you hit a repeating char then start from that position 
+        int i = 0; 
+        int j = 0; 
+        int maxWindow = 0 ; 
         HashMap<Integer, Integer> map = new HashMap<>();
-
         while (j < s.length()) {
-            int c = s.codePointAt(j);
-            int width = Character.charCount(c);
-
-            if (map.containsKey(c)) {
-                i = Math.max(i, map.get(c));
-            }
-
-            map.put(c, j + width);
-            maxWindow = Math.max(maxWindow, s.codePointCount(i, j + width));
-
-            j += width;
+          int c = s.codePointAt(j);
+           if(map.containsKey(c)){
+            i = Math.max(i, map.get(c) + 1);
+           }
+           map.put(c, j);
+           maxWindow = Math.max(maxWindow, j - i + 1);
+           j++;
         }
-
-        return maxWindow;
-    }
+    return maxWindow; 
+  }
 }
