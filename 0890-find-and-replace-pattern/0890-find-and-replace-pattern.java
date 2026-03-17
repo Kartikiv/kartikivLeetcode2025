83d647375e8e4786549a2b9a29adfec6d7f0304f
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-03-17 12:58:07 -0700

    Time: 1 ms (77.04%), Space: 43.6 MB (84.63%) - LeetHub

--- 0890-find-and-replace-pattern/0890-find-and-replace-pattern.java
+++ 0890-find-and-replace-pattern/0890-find-and-replace-pattern.java
@@ -1,25 +1,34 @@
 class Solution {
     public List<String> findAndReplacePattern(String[] words, String pattern) {
-        HashMap<String,List<String>> map = new HashMap<>();
+        List<String> ans = new ArrayList<>();
+        int [] patternMap = getSignature(pattern);
         for (String s : words){
-            String key = getSignature(s);
-            map.putIfAbsent(key, new ArrayList<>());
-            map.get(key).add(s);
+            if(s.length() != pattern.length()) {
+                continue;
+            }
+
+            int [] sMap = getSignature(s);
+            for(int i = 0 ; i < s.length(); i++){
+                if(sMap[s.charAt(i) - 'a'] != patternMap[pattern.charAt(i) - 'a']){
+                    break;
+                }
+                if(i == s.length() - 1) {
+                    ans.add(s);
+                }
+
+            }
+            
         }
-        pattern = getSignature(pattern);
-        List<String > ans = map.get(pattern);
-        return ans == null ? new ArrayList<>() : ans;
+    return ans; 
     }
 
-    String getSignature(String s) {
+    int [] getSignature(String s) {
         int[] map = new int[26];
-        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
             if (map[s.charAt(i) - 'a'] == 0) {
                 map[s.charAt(i) - 'a'] = i + 1;
             }
-            sb.append(map[s.charAt(i) - 'a']).append(":");
         }
-        return sb.toString();
+        return map;
     }
 }
