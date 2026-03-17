96dad906d3476dc5d5306d97818d3142980298d3
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-03-17 13:01:08 -0700

    Time: 1 ms (77.04%), Space: 43.7 MB (84.63%) - LeetHub

--- 0890-find-and-replace-pattern/0890-find-and-replace-pattern.java
+++ 0890-find-and-replace-pattern/0890-find-and-replace-pattern.java
@@ -1,34 +1,39 @@
 class Solution {
     public List<String> findAndReplacePattern(String[] words, String pattern) {
         List<String> ans = new ArrayList<>();
-        int [] patternMap = getSignature(pattern);
-        for (String s : words){
-            if(s.length() != pattern.length()) {
+
+        for (String word : words) {
+            if (word.length() != pattern.length()) {
                 continue;
             }
 
-            int [] sMap = getSignature(s);
-            for(int i = 0 ; i < s.length(); i++){
-                if(sMap[s.charAt(i) - 'a'] != patternMap[pattern.charAt(i) - 'a']){
+            int[] wordMap = new int[26];
+            int[] patternMap = new int[26];
+            boolean match = true;
+
+            for (int i = 0; i < word.length(); i++) {
+                int w = word.charAt(i) - 'a';
+                int p = pattern.charAt(i) - 'a';
+
+                if (wordMap[w] == 0) {
+                    wordMap[w] = i + 1;
+                }
+
+                if (patternMap[p] == 0) {
+                    patternMap[p] = i + 1;
+                }
+
+                if (wordMap[w] != patternMap[p]) {
+                    match = false;
                     break;
                 }
-                if(i == s.length() - 1) {
-                    ans.add(s);
-                }
-
             }
-            
-        }
-    return ans; 
-    }
 
-    int [] getSignature(String s) {
-        int[] map = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            if (map[s.charAt(i) - 'a'] == 0) {
-                map[s.charAt(i) - 'a'] = i + 1;
+            if (match) {
+                ans.add(word);
             }
         }
-        return map;
+
+        return ans;
     }
 }
