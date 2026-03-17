44ca9f5823bf39e2ab0e8550f4c640fbf240fdfd
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-03-17 15:45:44 -0700

    Time: 5 ms (86.77%), Space: 45.4 MB (83.52%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -8,12 +8,13 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap<>();
         while (j < s.length()) {
           int c = s.codePointAt(j);
+          int width = Character.charCount(c);
            if(map.containsKey(c)){
             i = Math.max(i, map.get(c) + 1);
            }
            map.put(c, j);
            maxWindow = Math.max(maxWindow, j - i + 1);
-           j++;
+           j = j + width;
         }
     return maxWindow; 
   }
