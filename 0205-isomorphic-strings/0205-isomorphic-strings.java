e3160edf0835fd00e147f265dc3ac8c97dde6102
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-03-17 09:02:05 -0700

    Time: 7 ms (78.75%), Space: 43.9 MB (54.05%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,28 +1,23 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        StringBuilder transformS = new StringBuilder();
-        StringBuilder transformT = new StringBuilder();
-        Map<Character, Integer> map = new HashMap<>();
+        int[]  sMap = getSignature(s);
+        int [] tMap = getSignature(t);
 
+        for(int i = 0 ; i < s.length(); i++){
+            if(sMap[s.charAt(i)] != tMap[t.charAt(i)]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    int[] getSignature(String s) {
+        int[] map = new int[256];
         for (int i = 0; i < s.length(); i++) {
-            if (!map.containsKey(s.charAt(i))) {
-                map.put(s.charAt(i), i);
-            }
-            transformS.append(map.get(s.charAt(i))).append(":");
+            if (map[s.charAt(i)] == 0)
+                map[s.charAt(i)] = i + 1;
+
         }
-
-        map.clear();
-
-        for (int i = 0; i < t.length(); i++) {
-            if (!map.containsKey(t.charAt(i))) {
-                map.put(t.charAt(i), i);
-            }
-            transformT.append(map.get(t.charAt(i))).append(":");
-        }
-
-        return transformS.toString().equals(transformT.toString());
+        return map;
     }
 }
