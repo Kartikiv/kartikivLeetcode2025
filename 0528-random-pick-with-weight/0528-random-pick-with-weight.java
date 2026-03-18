1857916b6ca4f9cf749f8fcade77756403522c25
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-03-18 16:27:07 -0700

    Time: 24 ms (95.22%), Space: 52 MB (40.02%) - LeetHub

--- 0528-random-pick-with-weight/0528-random-pick-with-weight.java
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.java
@@ -1,34 +1,32 @@
 class Solution {
-    int [] index;
-    HashMap<Integer,Integer> map;
+    int [] probalityDistribution;
+    int maxWeight ;    
     public Solution(int[] w) {
-       this.index = new int [10000];
-       this.map = new HashMap<>();
-       int sum = 0 ;
-
-        for(int i = 0 ; i < w.length ; i++){
-            sum += w[i];
-            map.put(w[i], i);
+        this.probalityDistribution = new int [w.length]; 
+        int sum = 0 ; 
+        for (int i = 0; i < w.length; i++) {
+            sum += w[i]; 
+            probalityDistribution[i] = sum ;
         }
-        int j = 0 ;
-        double probabilitySum = 0 ;
-        for(int i = 0 ; i < w.length ; i++){
-            probabilitySum += (double) w[i] / (double) sum;
-            int end = (int)(j + (((double) w[i] / (double) sum ) * 10000));
-            while(j < end){
-                index[j] = i;
-                j++;
+        // assign the max weight
+        this.maxWeight = sum; 
+
+    }
+    
+    public int pickIndex() {
+       int target = (int) (Math.random() * maxWeight) + 1;
+        int left = 0 ; 
+        int right = probalityDistribution.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;  
+            if(probalityDistribution[mid] >= target){
+                right = mid;
+            }else{
+                left = mid + 1; 
             }
         }
-        System.out.println(probabilitySum);
 
-    }
-
-    public int pickIndex() {
-        int num = (int)(Math.random() * 10000) - 1;
-        if(num < 0) num = 0;
-        return index[num];
-    }
+    return right; }
 }
 
 /**
