7a760ea0454a6771be6ccce2e34d612e9b3ac901
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-03-20 12:12:54 -0700

    Time: 4 ms (73.29%), Space: 45.6 MB (8.06%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -3,31 +3,31 @@ import java.util.Stack;
 
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-    // Hash num1 with indicies
-     HashMap<Integer, Integer> mapNum1 = new HashMap<>();
-     for (int i = 0; i < nums1.length; i++) {
-        mapNum1.put(nums1[i], i);
-        nums1[i] = -1;
-     }
+        // Hash num1 with indicies
+        HashMap<Integer, Integer> mapNum1 = new HashMap<>();
+        for (int i = 0; i < nums1.length; i++) {
+            mapNum1.put(nums1[i], i);
+            nums1[i] = -1;
+        }
 
-     // use a stack and the stack store elements in a decreasing order
-     // i.e push if only if peek > currentelement
-     Stack<Integer> stack = new Stack<>();
-     for (int i = 0; i < nums2.length; i++) {
-            if(stack.isEmpty()){
-                 if(mapNum1.containsKey(nums2[i])) stack.add(nums2[i]);
-            }else{
-                 while (!stack.isEmpty() && stack.peek() < nums2[i]) {
+        // use a stack and the stack store elements in a decreasing order
+        // i.e push if only if peek > currentelement
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < nums2.length; i++) {
+            if (stack.isEmpty()) {
+                if (mapNum1.containsKey(nums2[i]))
+                    stack.add(nums2[i]);
+            } else {
+                while (!stack.isEmpty() && stack.peek() < nums2[i]) {
                     int num = stack.pop();
                     nums1[mapNum1.get(num)] = nums2[i];
-                 }
-                 if(mapNum1.containsKey(nums2[i])) stack.add(nums2[i]);
-            
-       
+                }
+                if (mapNum1.containsKey(nums2[i]))
+                    stack.add(nums2[i]);
+
+            }
 
         }
-       
-     }
-   return nums1;
- }
+        return nums1;
+    }
 }
