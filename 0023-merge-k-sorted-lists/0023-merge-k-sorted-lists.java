4e881a17fcf3846380d72181fed78c9bb90861ca
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-03-21 14:35:40 -0700

    Time: 4 ms (80.91%), Space: 46.7 MB (83.78%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -1,53 +1,30 @@
+import java.util.*;
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
- // Brute Force
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists.length == 0) return null;
-        if(lists.length == 1) return lists[0];
-        else{
-            ListNode[] list = new ListNode[(lists.length + 1)/2]; 
-            int j = 0 ;
-            for (int i = 0 ; i < lists.length - 1; i = i + 2){
-                ListNode merge = mergeTwo(lists[i], lists[i + 1]);
-                list[j++] = merge;
+        Queue<ListNode> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
+        for (ListNode listNode : lists) {
 
-            }
-            if(lists.length != 1 && lists.length % 2 != 0){
-                list[j] = lists[lists.length - 1];
-            }
-           return mergeKLists(list);
-           }
-        
-            }
-
-    public ListNode mergeTwo(ListNode list1, ListNode list2) {
-        ListNode head = new ListNode(-1);
-        ListNode dummy = head;
-        while (list1 != null && list2 != null) {
-            if (list1.val < list2.val) {
-                dummy.next = list1;
-                dummy = dummy.next;
-                list1 = list1.next;
-            } else {
-                dummy.next = list2;
-                dummy = dummy.next;
-                list2 = list2.next;
-            }
+            if(listNode != null) pq.add(listNode);
         }
-        if(list1 != null){
-            dummy.next = list1;
-        }else{
-            dummy.next = list2;
+        ListNode ans = new ListNode(-1);
+        ListNode dummy = ans;
+        while (!pq.isEmpty()) {
+            ListNode node = pq.poll();
+            dummy.next = new ListNode(node.val);
+            node = node.next; 
+            if(node != null) pq.add(node);
+            dummy = dummy.next;
         }
-    return head.next;
-    }
+    return ans.next; }
 }
