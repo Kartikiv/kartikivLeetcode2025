ea6d464ab6139b36d0c4ca9944c817c48f4a5b7c
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-03-21 19:50:36 -0700

    Time: 0 ms (100%), Space: 46.8 MB (45.1%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -1,37 +1,26 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    // we can use controlled inorder travesal\
-    int ans ;
-    int k ;
+    int ans;
+    int k;
+
     public int kthSmallest(TreeNode root, int k) {
-        this.ans = Integer.MAX_VALUE; 
-        this.k = k ;
+        this.k = k;
         helper(root);
         return ans;
     }
-    void helper(TreeNode root){
-        if(root == null ) return ;
+
+    void helper(TreeNode root) {
+        if (root == null || k == 0) return;
+
         helper(root.left);
-        if(k > 0){
+
+        if (k == 0) return;
+
+        k--;
+        if (k == 0) {
             ans = root.val;
-            k--;
-        }else{
-            return; 
+            return;
         }
+
         helper(root.right);
     }
 }
