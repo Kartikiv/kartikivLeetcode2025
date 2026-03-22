0f30f5ff2891811681661c360e7e459755b41890
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-03-22 12:27:08 -0700

    Time: 0 ms (100%), Space: 44.2 MB (75.35%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -1,57 +1,41 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if(head == null) return null;
+        if(head == null) return head; 
+        // go to the middle node and reverse the linked list 
+        // then interweave the two node
+        // lets to brute force first
         ListNode sizeNode = head;
-        int size = 0;
-        while(sizeNode != null){
+        int size = 0 ; 
+        while (sizeNode != null) {
             size++;
             sizeNode = sizeNode.next;
         }
         k = k % size;
-        if(k == 0 ) return head;
-        ListNode temp = head;
-        ListNode curr = head;
+        if(k == size || k == 0 ) return head;
+        
+        ListNode firstNode = head; 
+        ListNode secondNode = head; 
         while (k > 0) {
-            temp = temp.next;
+            secondNode = secondNode.next;
             k--;
-
         }
-
-        while (temp != null && temp.next != null){
-            temp = temp.next;
-            curr = curr.next;
+        while(secondNode != null && secondNode.next != null){
+            secondNode = secondNode.next; 
+            firstNode = firstNode.next;
         }
-        temp = curr.next;
-        curr.next = null;
-        curr = temp;
-        while (curr != null && curr.next!= null){
-            curr = curr.next;
-        }
-        curr.next = head;
-
-
-
-        return temp;}
-
-    ListNode reverse(ListNode node){
-        ListNode prev = null;
-        ListNode curr = node;
-        ListNode next = null;
-        while(curr != null){
-            next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return  prev;}
+        ListNode reversedNode = firstNode.next; 
+        firstNode.next = null;
+        secondNode.next = head;
+        return reversedNode;
+}
 }
