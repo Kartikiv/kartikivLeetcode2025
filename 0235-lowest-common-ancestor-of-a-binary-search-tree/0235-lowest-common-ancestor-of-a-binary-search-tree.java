d842a6bba72dba5a928d0b5c795b1deabfec5627
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-03-22 14:24:53 -0700

    Time: 6 ms (97.13%), Space: 47.9 MB (11.76%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -1,30 +1,17 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null) return null;
-        if(root.val == p.val || root.val == q.val) return root;
-        // serch left
-        if(root.val > p.val && root.val > q.val) {
-            return lowestCommonAncestor(root.left, p, q);
-        // search right
-        }else if(root.val < p.val && root.val < q.val){
-              return lowestCommonAncestor(root.right, p, q);
-        // overlap found 
-        }else {
-            TreeNode left = lowestCommonAncestor(root.left, p, q);
-            TreeNode right = lowestCommonAncestor(root.right, p, q);
-            if(left != null && right != null) return root;
-            return left == null ? right : left;
+        TreeNode curr = root;
+
+        while (curr != null) {
+            if (p.val < curr.val && q.val < curr.val) {
+                curr = curr.left;
+            } else if (p.val > curr.val && q.val > curr.val) {
+                curr = curr.right;
+            } else {
+                return curr;
+            }
         }
 
-}
+        return null;
+    }
 }
