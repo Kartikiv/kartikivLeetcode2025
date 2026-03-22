51b801a87fbadf7aaab0410d6a3c974a93ddf42a
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-03-22 14:12:49 -0700

    Time: 6 ms (97.13%), Space: 47.5 MB (84.61%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -10,14 +10,21 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null) return root;
-        if(root == p || root == q)  return root;
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
-        if(left != null && right != null) return root;
-        
-        
+        if(root == null) return null;
+        if(root.val == p.val || root.val == q.val) return root;
+        // serch left
+        if(root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        // search right
+        }else if(root.val < p.val && root.val < q.val){
+              return lowestCommonAncestor(root.right, p, q);
+        // overlap found 
+        }else {
+            TreeNode left = lowestCommonAncestor(root.left, p, q);
+            TreeNode right = lowestCommonAncestor(root.right, p, q);
+            if(left != null && right != null) return root;
+            return left == null ? right : left;
+        }
 
-return left == null ? right : left; 
 }
 }
