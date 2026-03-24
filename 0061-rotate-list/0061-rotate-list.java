e3b6ddc053718802878c69b10aa11eb87d3ca0bf
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-03-24 13:57:42 -0700

    Time: 0 ms (100%), Space: 44.5 MB (37.18%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -10,32 +10,23 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if(head == null) return head; 
-        // go to the middle node and reverse the linked list 
-        // then interweave the two node
-        // lets to brute force first
-        ListNode sizeNode = head;
-        int size = 0 ; 
-        while (sizeNode != null) {
-            size++;
-            sizeNode = sizeNode.next;
+        // normalize k
+        if(head == null) return null;
+        int size = 1;
+        ListNode tail = head;
+        while(tail.next != null){ 
+            size++; 
+            tail = tail.next;
         }
         k = k % size;
-        if(k == size || k == 0 ) return head;
-        
-        ListNode firstNode = head; 
-        ListNode secondNode = head; 
-        while (k > 0) {
-            secondNode = secondNode.next;
-            k--;
+        tail.next = head;
+        int breakPoint = size - k;
+        while (breakPoint > 0) {
+            tail = tail.next;
+            breakPoint--;
         }
-        while(secondNode != null && secondNode.next != null){
-            secondNode = secondNode.next; 
-            firstNode = firstNode.next;
-        }
-        ListNode reversedNode = firstNode.next; 
-        firstNode.next = null;
-        secondNode.next = head;
-        return reversedNode;
-}
+        ListNode ans = tail.next; 
+        tail.next = null;
+    return ans; 
+    }
 }
