cc9ef7acdf56f12b84cacddc030a48571de591ea
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-03-24 20:48:33 -0700

    Time: 5 ms (29.35%), Space: 51.1 MB (98.12%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,45 +1,56 @@
-import java.util.LinkedList;
-import java.util.Queue;
-class Node {
-    int i ;
-    int j ;
-    public Node(int i , int j){
-        this.i = i;
+class Node{ 
+    int i ; 
+    int j ; 
+    public Node(int i, int j){ 
+        this.i = i; 
         this.j = j;
     }
 }
 class Solution {
     public int numIslands(char[][] grid) {
-        int count = 0;
-    for(int i = 0 ; i < grid.length ; i++){
-        for (int j = 0 ; j < grid[0].length ; j++){
-            if (grid[i][j] == '1'){
-                bfs(grid, i ,j);
-                count++;
-            }
-        }
-    }
-   return count;
-    }
-    void bfs(char[][] grid, int i , int j){
-        int [][] directions = new int [][] {{-1,0}, {0,-1}, {0,1}, {1,0}};
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(new Node(i,j));
-        grid[i][j] = 0;
-        while (!queue.isEmpty()){
-            Node node = queue.poll();
-            for (int [] direction : directions){
-                int newi = node.i + direction[0];
-                int newj = node.j + direction[1];
-                if(newi >= 0 && newi < grid.length 
-                    && newj >= 0 && newj <grid[0].length 
-                    && grid[newi][newj] == '1'){
-                    queue.add(new Node(newi, newj));
-                    grid[newi][newj] = 0 ;
+        int count = 0 ;
+        int m = grid.length; 
+        int n = grid[0].length;
+        for(int i =0 ; i < m ; i++){ 
+            for(int j = 0 ; j < n ; j++){
+                if(grid[i][j] == '1') {
+                    bfs(grid, new Node(i, j));
+                    count++;
                 }
-
             }
         }
 
+   return count; 
     }
+    public void bfs(char[][] grid, Node root){ 
+        int m = grid.length; 
+        int n = grid[0].length;
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root); 
+        grid[root.i][root.j] = '0';
+        int [][] directions = new int [][]{{0,1} , {1,0}, {0, -1}, {-1,0}};
+        while(!queue.isEmpty()){ 
+            Node node = queue.poll();
+            int i = node.i; 
+            int j = node.j;
+            // insert valid children 
+            for(int [] direction : directions){ 
+                int newi = i + direction[0];
+                int newj = j + direction[1];
+                if(newi >= 0 && newj >= 0 
+                    && newi < m && newj < n 
+                    && grid[newi][newj] == '1'
+                ){ 
+                   // mark visited
+                    grid[newi][newj] = '0';
+                    queue.add(new Node(newi , newj));
+                }
+            }
+
+
+            
+        }
+
+    }
+    
 }
