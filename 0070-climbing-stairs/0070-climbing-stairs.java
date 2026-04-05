e8c0e4714f01d838fd63524485087976d7cbf208
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-04-04 22:55:35 -0700

    Time: 0 ms (100%), Space: 42 MB (62.98%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,12 +1,13 @@
 class Solution {
     public int climbStairs(int n) {
-        int [] dp = new int[n + 1];
+        // We need to develop a recursive relationship between nth stair 
+        if(n < 1) return 0;
+        int [] dp = new int [n + 1];
         dp[0] = 1;
-        dp[1] = 1; 
-        for(int i = 2; i <= n; i++){ 
-            dp[i] = dp[i- 1] + dp[i - 2];
-        } 
-        
-    return dp[n];
+        dp[1] = 1;  
+        for(int i =2 ; i < dp.length ; i++){ 
+            dp[i] = dp[i - 1] + dp[i -2];
+        }
+        return dp[dp.length -1];
     }
 }
