35e4b0cbe2d8cbc93409dde6b0eae44f31895e73
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-04-15 16:48:52 -0700

    Time: 1 ms (93.21%), Space: 42.6 MB (72.57%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -1,8 +1,5 @@
 class Solution {
     public int divide(int dividend, int divisor) {
-        if (dividend == 0) return 0;
-
-        // overflow case
         if (dividend == Integer.MIN_VALUE && divisor == -1) {
             return Integer.MAX_VALUE;
         }
@@ -11,25 +8,22 @@ class Solution {
         long b = Math.abs((long) divisor);
 
         int sign = ((dividend < 0) ^ (divisor < 0)) ? -1 : 1;
+        long result = 0;
 
-        long left = 0;
-        long right = a;
+        while (a >= b) {
+            long temp = b;
+            long multiple = 1;
 
-        while (left < right) {
-            long mid = left + (right - left + 1) / 2;
-
-            if (mid * b <= a) {
-                left = mid;
-            } else {
-                right = mid - 1;
+            while ((temp << 1) <= a) {
+                temp <<= 1;
+                multiple <<= 1;
             }
+
+            a -= temp;
+            result += multiple;
         }
 
-        long ans = sign * left;
-
-        if (ans > Integer.MAX_VALUE) return Integer.MAX_VALUE;
-        if (ans < Integer.MIN_VALUE) return Integer.MIN_VALUE;
-
-        return (int) ans;
+        result = sign * result;
+        return (int) result;
     }
 }
