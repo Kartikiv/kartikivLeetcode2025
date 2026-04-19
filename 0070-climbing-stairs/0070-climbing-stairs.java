6eb77f7d5a1a438d92d757587e0cebe7bac39e46
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-04-19 10:49:51 -0700

    Time: 0 ms (100%), Space: 42.3 MB (6.83%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,17 +1,23 @@
 class Solution {
     public int climbStairs(int n) {
-        int secondPreviousStep = 1;
-        int firstPreviousStep = 1;
-        if(n == 1) return firstPreviousStep;
-        int ans = 0;
+        return (int) fib(n + 1)[0];
+    }
 
-        
-        for (int i = 2; i < n + 1; i++) {
-            ans = firstPreviousStep + secondPreviousStep;
-            secondPreviousStep = firstPreviousStep;
-            firstPreviousStep = ans;
+    // returns {F(n), F(n+1)}
+    private long[] fib(int n) {
+        if (n == 0) return new long[]{0, 1};
+
+        long[] half = fib(n / 2);
+        long a = half[0]; // F(k)
+        long b = half[1]; // F(k+1)
+
+        long c = a * (2 * b - a);      // F(2k)
+        long d = a * a + b * b;        // F(2k+1)
+
+        if (n % 2 == 0) {
+            return new long[]{c, d};
+        } else {
+            return new long[]{d, c + d};
         }
-
-    return ans;
     }
 }
