f20ab01f450f2f78df8f08808dc7a91534a228ec
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-04-22 18:37:53 -0700

    Time: 26 ms (0.4%), Space: 46.7 MB (6.67%) - LeetHub

---
+++ 0832-flipping-an-image/0832-flipping-an-image.java
@@ -0,0 +1,46 @@
+class MyThread extends Thread {
+    int[] row;
+
+    public MyThread(int[] row) {
+        this.row = row;
+    }
+
+    @Override
+    public void run() {
+        // flip 
+        int i = 0;
+        int j = row.length - 1;
+        while (i < j) {
+            int temp = row[i];
+            row[i] = row[j];
+            row[j] = temp;
+            i++;
+            j--;
+        }
+        // invert 
+        for (int k = 0; k < row.length; k++) {
+            row[k] = row[k] ^ 1;
+        }
+    }
+
+}
+
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image){
+        // this is heavily parallalizable 
+        for (int i = 0; i < image.length; i++) {
+            try{
+            MyThread newThread = new MyThread(image[i]);
+            newThread.start();
+            newThread.join();
+            image[i] = newThread.row;
+            }
+            catch(Exception e){
+
+            }
+        }
+
+        return image;
+    }
+
+}
