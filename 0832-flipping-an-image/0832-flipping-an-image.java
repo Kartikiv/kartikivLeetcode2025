74ca71b64f0e8ddc92704c7ffe6aa6849780e55f
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-04-22 18:39:40 -0700

    Time: 0 ms (100%), Space: 45.7 MB (73.54%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.java
+++ 0832-flipping-an-image/0832-flipping-an-image.java
@@ -1,49 +1,13 @@
-class MyThread extends Thread {
-    int[] row;
-
-    public MyThread(int[] row) {
-        this.row = row;
-    }
-
-    @Override
-    public void run() {
-        // flip 
-        int i = 0;
-        int j = row.length - 1;
-        while (i < j) {
-            int temp = row[i];
-            row[i] = row[j];
-            row[j] = temp;
-            i++;
-            j--;
-        }
-        // invert 
-        for (int k = 0; k < row.length; k++) {
-            row[k] = row[k] ^ 1;
-        }
-    }
-
-}
-
 class Solution {
-    public int[][] flipAndInvertImage(int[][] image){
-        MyThread[] threads = new MyThread[image.length];
-
-        for (int i = 0; i < image.length; i++) {
-            threads[i] = new MyThread(image[i]);
-            threads[i].start();
-        }
-
-        for (int i = 0; i < image.length; i++) {
-            try {
-                threads[i].join();
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                throw new RuntimeException(e);
+    public int[][] flipAndInvertImage(int[][] A) {
+        int C = A[0].length;
+        for (int[] row: A)
+            for (int i = 0; i < (C + 1) / 2; ++i) {
+                int tmp = row[i] ^ 1;
+                row[i] = row[C - 1 - i] ^ 1;
+                row[C - 1 - i] = tmp;
             }
-        }
 
-        return image;
+        return A;
     }
-
 }
