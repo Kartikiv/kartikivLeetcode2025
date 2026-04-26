9bc847b2a818683bca8cd6a939be68c4b3392ed4
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-04-25 22:41:31 -0700

    Time: 0 ms (100%), Space: 42.9 MB (42.36%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,26 +1,27 @@
 class Solution {
     public int rob(int[] nums) {
-        int len = nums.length;
-        if(len == 1) return nums[0];
-        if(len == 2) return Math.max(nums[0], nums[1]);
-        int firstPart [] = Arrays.copyOfRange(nums, 0, len - 1);
-        int secondPart [] = Arrays.copyOfRange(nums, 1, len );
+        if (nums.length == 1)
+            return nums[0];
+        if (nums.length == 2)
+            return Math.max(nums[0], nums[1]);
 
-        return Math.max(rob1(firstPart), rob1(secondPart));
-    }
-    public int rob1(int[] nums) {
-        
-        int len = nums.length;
-        if(len == 1) return nums[0];
-        if(len == 2) return Math.max(nums[0], nums[1]);
-        int dp [] = new int [len];
-        int maxRob = 0;
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-        for (int i = 2; i < len; i++){
-            dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
-            maxRob = Math.max(maxRob, dp[i]);
+        int secondPreviousHouse = nums[0];
+        int previousHouse = Math.max(nums[0], nums[1]);
+
+        int secondPreviousHouse1 = 0;
+        int previousHouse1 = nums[1];
+
+        for (int i = 2; i < nums.length; i++) {
+            int temp = previousHouse;
+            if(i < nums.length - 1){
+            previousHouse = Math.max(previousHouse, secondPreviousHouse + nums[i]);
+            secondPreviousHouse = temp;
+            }
+
+            temp = previousHouse1;
+            previousHouse1 = Math.max(previousHouse1, secondPreviousHouse1 + nums[i]);
+            secondPreviousHouse1 = temp;
         }
-    return maxRob; 
+        return Math.max(previousHouse1, previousHouse);
     }
 }
