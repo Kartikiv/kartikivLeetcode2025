664f83121bc91a84319746fd7f96dd2320fb1326
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-04-30 12:13:06 -0700

    Time: 643 ms (8.1%), Space: 42.8 MB (96.35%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,31 +1,16 @@
 class Solution {
-    int count = 0 ;
-    HashMap<Pair<Integer, Integer>, Integer> memo = new HashMap<>();
     public int findTargetSumWays(int[] nums, int target) {
-    return backTracking(nums, 0, target);
+        
+    return backtrack(0, nums, target); 
     }
-    public int backTracking(int [] nums , int index, int target){
-         if (index == nums.length) {
-            if (target == 0) {
-                return 1;
-            }
-            return 0;
+    public int backtrack(int index, int [] nums, int target){ 
+        if(nums.length == index){ 
+            return target == 0 ? 1 : 0; 
         }
-        Pair<Integer, Integer> key = new Pair<>(index, target);
-        Integer cached = memo.get(key);
-        if (cached != null) return cached;
-
-        /*
-         Choice 1: put '+' before nums[index]  => remaining target decreases
-         Choice 2: put '-' before nums[index]  => remaining target increases
-        */
-        int ways =
-            backTracking(nums, index + 1, target - nums[index]) +
-            backTracking(nums, index + 1, target + nums[index]);
-
-        memo.put(key, ways);
-        return ways;
-
-
-    }
+        // here i backtrack 
+        int add = backtrack(index + 1, nums, target + nums[index]); // subtract number
+        int subtract = backtrack(index + 1, nums, target - nums[index]); // add number 
+        
+        return add + subtract; 
+        }
 }
