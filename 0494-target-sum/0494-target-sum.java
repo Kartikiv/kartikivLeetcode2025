28b3daef132e82a145865d71f0a9990ab047c593
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-05-01 02:03:39 -0700

    Time: 632 ms (15.99%), Space: 43.1 MB (66.42%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,16 +1,18 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        
-    return backtrack(0, nums, target); 
+
+        return backtrack(nums, target, 0);
     }
-    public int backtrack(int index, int [] nums, int target){ 
-        if(nums.length == index){ 
-            return target == 0 ? 1 : 0; 
-        }
-        // here i backtrack 
-        int add = backtrack(index + 1, nums, target + nums[index]); // subtract number
-        int subtract = backtrack(index + 1, nums, target - nums[index]); // add number 
-        
-        return add + subtract; 
-        }
+
+    public int backtrack(int[] nums, int target, int index) {
+        if (index == nums.length) {
+            return target == 0 ? 1 : 0;
+        } // found a path
+
+        // backtrack choices
+        int add = backtrack(nums, target - nums[index], index + 1);
+        int subtract = backtrack(nums, target + nums[index], index + 1);
+
+        return add + subtract;
+    }
 }
