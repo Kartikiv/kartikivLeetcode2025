61fb43318e5ac7ec3da919ef6fe9fe2bd756ff19
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-05-01 15:04:24 -0700

    Time: 105 ms (11.86%), Space: 75.3 MB (21.19%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,24 +1,38 @@
-// TLE Solution using backtracking 
+
+import java.util.Arrays;
+
 class Solution {
-    int [] nums; 
+    int[][] memo;
+
+    // we need to partion the array into equal sums
     public boolean canPartition(int[] nums) {
-        this.nums = nums;
-        int sum = 0; 
-        for(int num : nums){
-            sum += num;
-        }
-        if(sum % 2 == 1 ) return false;
-        sum = sum / 2 ;
-        Boolean[][] memo = new Boolean[nums.length + 1][sum + 1];
-        return dfs(nums.length - 1 , sum, memo);
-    }
-    public boolean dfs(int n , int sum, Boolean[][] memo){
-        if( sum == 0 ) return true;
-        if (n == 0 || sum < 0)
+        int sum = Arrays.stream(nums).sum();
+        this.memo = new int[nums.length + 1][sum / 2 + 1];
+        if (sum % 2 != 0) {
             return false;
-        if(memo[n][sum] != null ) return memo[n][sum];
-        boolean res = dfs(n - 1, sum - nums[n - 1], memo) || dfs(n - 1 , sum, memo); 
-        memo[n][sum] = res;
-        return memo[n][sum];
+        }
+        return bactrack(0, nums, 0 ,sum / 2);
+    }
+
+    public boolean bactrack(int index, int[] nums, int sum, int target) {
+        if(target < sum) {
+           return false;
+        }
+        if (memo[index][sum] != 0) {
+            return memo[index][sum] == 1;
+        }
+        if (sum == target) { // reached a path where the sum is exactly half of the total sum
+            return true;
+        }
+        if (nums.length == index) {
+            return false;
+        }
+        // i include or not include
+        // include
+        boolean include = bactrack(index + 1, nums, sum + nums[index], target);
+        // exclude
+        boolean exclude = bactrack(index + 1, nums, sum, target);
+        memo[index][sum] = include || exclude == true ? 1 : 2;
+        return memo[index][sum] == 1;
     }
 }
