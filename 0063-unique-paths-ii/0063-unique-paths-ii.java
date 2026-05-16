b87505b5d915ffe5123e40e3136079cdbf52b3b7
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-05-15 20:26:08 -0700

    Time: 4 ms (4.01%), Space: 43.1 MB (97.59%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if(obstacleGrid[0][0] == 1) return 0;
+        if (obstacleGrid.length == 1 && obstacleGrid[0].length == 1) return obstacleGrid[0][0] == 1 ? 0 : 1;
+     helperDfs(obstacleGrid, 0, 0);
+     return -1 * obstacleGrid[0][0];
+    }
+
+    public int helperDfs(int[][] obstacleGrid, int i, int j) {
+   
+        if (i >= obstacleGrid.length || j >= obstacleGrid[0].length) {
+            return 0;
+        }
+        if (i == obstacleGrid.length - 1 && j == obstacleGrid[0].length - 1) {
+            return obstacleGrid[i][j] == 1 ? 0 : -1;
+        }
+        if (obstacleGrid[i][j] == 1) {
+            return 0;
+        }
+             if (obstacleGrid[i][j] < 0) {
+    return obstacleGrid[i][j];
+}
+        obstacleGrid[i][j] = helperDfs(obstacleGrid, i + 1, j) + helperDfs(obstacleGrid, i, j + 1);
+
+        return obstacleGrid[i][j];
+
+    }
+}
