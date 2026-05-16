587af5880e98fde3285bc37e408c44ca017248e5
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-05-15 20:27:48 -0700

    Time: 4 ms (4.01%), Space: 43.7 MB (10.75%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,30 +1,37 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        if (obstacleGrid[0][0] == 1)
+        if (obstacleGrid[0][0] == 1) {
             return 0;
-        if (obstacleGrid.length == 1 && obstacleGrid[0].length == 1)
-            return obstacleGrid[0][0] == 1 ? 0 : 1;
-        helperDfs(obstacleGrid, 0, 0);
-        return -1 * obstacleGrid[0][0];
+        }
+
+        int ans = helperDfs(obstacleGrid, 0, 0);
+        return -ans;
     }
 
     public int helperDfs(int[][] obstacleGrid, int i, int j) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
 
-        if (i >= obstacleGrid.length || j >= obstacleGrid[0].length) {
+        if (i >= m || j >= n) {
             return 0;
         }
-        if (i == obstacleGrid.length - 1 && j == obstacleGrid[0].length - 1) {
-            return obstacleGrid[i][j] == 1 ? 0 : -1;
-        }
+
         if (obstacleGrid[i][j] == 1) {
             return 0;
         }
+
         if (obstacleGrid[i][j] < 0) {
             return obstacleGrid[i][j];
         }
-        obstacleGrid[i][j] = helperDfs(obstacleGrid, i + 1, j) + helperDfs(obstacleGrid, i, j + 1);
+
+        if (i == m - 1 && j == n - 1) {
+            return -1;
+        }
+
+        obstacleGrid[i][j] =
+            helperDfs(obstacleGrid, i + 1, j) +
+            helperDfs(obstacleGrid, i, j + 1);
 
         return obstacleGrid[i][j];
-
     }
 }
