4b9fdf4f98f08ee0c2e37c6be4a965827cc5e9ef
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-05-23 14:31:21 -0700

    Time: 1 ms (99.97%), Space: 42.9 MB (12.1%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -4,11 +4,13 @@ class Solution {
         int sign = x > 0 ? 1 : -1;
         x = x > 0 ? x : -1 * x;
         while (x > 0) {
-            if(rev > Integer.MAX_VALUE / 10 ) {
-                 return 0; 
+            if (rev > Integer.MAX_VALUE / 10) {
+                return 0;
             }
-            if (sign == 1 && rev == Integer.MAX_VALUE / 10 && x % 10  > 7) return 0;
-            if (sign == -1 && rev == Integer.MAX_VALUE / 10 && x % 10  >  8) return 0;
+            if (sign == 1 && rev == Integer.MAX_VALUE / 10 && x % 10 > 7)
+                return 0;
+            if (sign == -1 && rev == Integer.MAX_VALUE / 10 && x % 10 > 8)
+                return 0;
             rev *= 10;
             rev += x % 10;
             x = x / 10;
