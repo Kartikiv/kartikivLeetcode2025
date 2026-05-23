ce86001415bffce204f34e9531f07323308d31f4
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-05-22 22:45:49 -0700

    Time: 2 ms (99.65%), Space: 42.6 MB (95.54%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String countAndSay(int n) {
+        if(n <= 0) return "";
+        if (n == 1) {
+            return "1";
+        }
+        
+       
+        String previousTerm = countAndSay(n - 1);
+        
+    
+        return applyRLE(previousTerm);
+    }
+    
+    
+    private static String applyRLE(String s) {
+        StringBuilder result = new StringBuilder();
+        int count = 1;
+        
+      
+        for (int i = 1; i < s.length(); i++) {
+            
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                count++;
+            } else {
+               
+                result.append(count).append(s.charAt(i - 1));
+                
+                count = 1;
+            }
+        }
+        
+        result.append(count).append(s.charAt(s.length() - 1));
+        
+        return result.toString();
+    }
+}
