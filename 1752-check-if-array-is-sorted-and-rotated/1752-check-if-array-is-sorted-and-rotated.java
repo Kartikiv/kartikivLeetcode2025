9dea14fda6dbec96da0a12bd625efeb0c0b00795
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-05-22 21:47:03 -0700

    Time: 0 ms (100%), Space: 42.6 MB (94.96%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,31 +1,19 @@
 class Solution {
     public boolean check(int[] nums) {
-        // base case
-        if(nums.length == 1) return true;
-        // if an array is sorted and rotated 
-        // what are the contidions that remain true ?? 
-        // rotation means we encounter a pivot i.e we encounter a smaller number in the sequence 
-        // after the encoiuntering the pivot all the elements after the pivot are smaller that the largest number 
-        int maxSoFar = nums[0];
-        int minSoFar = nums[0];
-        int previousNumber = nums[0];
-        boolean encounteredPivot = false;
+        int drops = 0;
+
         for (int i = 0; i < nums.length; i++) {
-            // if nums[i] > previous number go on
-            if(nums[i] < previousNumber){ 
-                if (encounteredPivot) return false; 
-                encounteredPivot = true; 
-                
-            } if(encounteredPivot && (nums[i] > maxSoFar || nums[i] > minSoFar)){ 
-                return false;
-            }else { 
-                if(!encounteredPivot){ 
-                    maxSoFar = nums[i];
-                    
-                }
+            int next = (i + 1) % nums.length;
+
+            if (nums[i] > nums[next]) {
+                drops++;
             }
-            previousNumber = nums[i];
+
+            if (drops > 1) {
+                return false;
+            }
         }
-    return true; 
+
+        return true;
     }
 }
