5db0391db323c0e3b46bb857f5b458d0eb4c89bf
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-05-26 21:30:38 -0700

    Time: 2 ms (99.27%), Space: 44.4 MB (65.4%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,21 +1,34 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        
-        int i  = 0 ;
-        int j = s.length() -1 ;
-        while (i < j) {
-            while( i < j && !Character.isLetterOrDigit(s.charAt(i)) ) {i++;}
-            while( i < j && !Character.isLetterOrDigit(s.charAt(j)) ){j--;}
-        if( Character.toLowerCase( s.charAt(i) ) !=  Character.toLowerCase(s.charAt(j))) {
+        // compare without altering the string 
+        // one all non alphanumneric characters are ignored 
+        // range we are looking for a - z , A - Z, 0 - 9. 
+        int i = 0 ; 
+        int j = s.length() - 1; 
+        while(i < j){ 
+            while (i < j && !checkRange(s.charAt(i))) {
+                i++;
+            }
+            while (i < j && !checkRange(s.charAt(j))) {
+                j--;
+            }
+            // normalize 
+            char c = s.charAt(i) >= 'A' && s.charAt(i) <= 'Z' ? (char)(s.charAt(i) + 32) : s.charAt(i); 
+            char d = s.charAt(j) >= 'A' && s.charAt(j) <= 'Z' ? (char)(s.charAt(j) + 32) : s.charAt(j);
+            // compare 
+            if(c != d){ 
                 return false;
             }
-            i++;
+            i++; 
             j--;
         }
-        return true;
+    return true; 
+    }
+    public boolean checkRange(char c){ 
+        if(c < 48) return false; 
+        if(c > 57 && c < 65) return false; 
+        if (c > 90 && c < 97) return false; 
+        if(c > 122) return false; 
+    return true; 
     }
 }
-// Notes 582
-/*
-
-*/
