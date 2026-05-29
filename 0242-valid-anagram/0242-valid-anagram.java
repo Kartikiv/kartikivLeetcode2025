94a75999afcbc94cc1a9b0a3c51e5922e4a5abf8
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-05-29 15:30:02 -0700

    Time: 4 ms (82.38%), Space: 44.3 MB (81.9%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,18 +1,19 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int [] freqMap = new int [26];
-        for(int i = 0 ; i < s.length(); i++){ 
+        int[] freqMap = new int[26];
+        for (int i = 0; i < s.length(); i++) {
             // increment the freq 
-            freqMap[s.charAt(i) - 'a']++; 
-        } 
-
-        for(int i = 0 ; i < t.length(); i++){ 
-            // increment the freq 
-            freqMap[t.charAt(i) - 'a']--; 
-        } 
-        for(int num : freqMap){ 
-            if(num != 0 ) return false; 
+            freqMap[s.charAt(i) - 'a']++;
         }
-    return true; 
+
+        for (int i = 0; i < t.length(); i++) {
+            // increment the freq 
+            freqMap[t.charAt(i) - 'a']--;
+        }
+        for (int num : freqMap) {
+            if (num != 0)
+                return false;
+        }
+        return true;
     }
 }
