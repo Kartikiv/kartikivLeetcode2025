b13ab43d170bcaec39e830139d8effd3e4091869
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-05-29 15:29:43 -0700

    Time: 4 ms (82.38%), Space: 44.1 MB (88.91%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,27 +1,18 @@
 class Solution {
-  public static boolean isAnagram(String s, String t) {
-    // anagram are words with same character and frequency but there are jumbbled
-    // For example dog , god there are anagram
-    // Simple approach is check with two arrays on 26 length and return if all the
-    // elements are zero
-    // so only one array of 26 is enough
-    int[] charArr = new int[26];
-    if (s.length() != t.length())
-      return false;
-    for (int i = 0; i < s.length(); i++) {
-      charArr[s.charAt(i) - 'a']++;
-      charArr[t.charAt(i) - 'a']--;
+    public boolean isAnagram(String s, String t) {
+        int [] freqMap = new int [26];
+        for(int i = 0 ; i < s.length(); i++){ 
+            // increment the freq 
+            freqMap[s.charAt(i) - 'a']++; 
+        } 
+
+        for(int i = 0 ; i < t.length(); i++){ 
+            // increment the freq 
+            freqMap[t.charAt(i) - 'a']--; 
+        } 
+        for(int num : freqMap){ 
+            if(num != 0 ) return false; 
+        }
+    return true; 
     }
-
-    for (int num : charArr) {
-      if (num != 0)
-        return false;
-    }
-
-    return true;
-  }
-
-  public static void main(String [] args){
-    System.out.println(isAnagram("anagram","nagaram"));
-  }
 }
