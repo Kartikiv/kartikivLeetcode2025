14b94026f1d6b74cd99ab425cf5aeb431a2f4f5f
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-05-29 23:04:25 -0700

    Time: 174 ms (67.06%), Space: 108.2 MB (10%) - LeetHub

--- 1726-tuple-with-same-product/1726-tuple-with-same-product.java
+++ 1726-tuple-with-same-product/1726-tuple-with-same-product.java
@@ -1,19 +1,19 @@
 class Solution {
     public int tupleSameProduct(int[] nums) {
-      // create a product map 
-      HashMap<Integer,Integer> productMap = new HashMap<>(); 
-      for(int i = 0 ; i < nums.length; i++){ 
-        for (int j = i + 1; j < nums.length; j++){ 
-           productMap.put(nums[i] * nums[j], productMap.getOrDefault(nums[i] * nums[j], 0) + 1);
+        // create a product map 
+        HashMap<Integer, Integer> productMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                productMap.put(nums[i] * nums[j], productMap.getOrDefault(nums[i] * nums[j], 0) + 1);
+            }
         }
-      }
-      int ans = 0 ; 
-      for(int key : productMap.keySet()){
-        int n = productMap.get(key);
-        ans += (n * (n - 1)) * 4;
-      } 
+        int ans = 0;
+        for (int key : productMap.keySet()) {
+            int n = productMap.get(key);
+            ans += (n * (n - 1)) * 4;
+        }
 
-
-    return ans; }
+        return ans;
+    }
 }
 // [1,2] 3 * 2 * 2 * 2
