e2e3f23b160f8306ed91c95b11dc8fabe33ea6ae
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-05-30 20:38:10 -0700

    Time: 24 ms (76.77%), Space: 48.6 MB (70.57%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,19 +1,18 @@
-
-import java.util.HashMap;
-
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        // count empty subarray
-        map.put(0, 1); 
-        int count = 0; 
-        int sum = 0;
-        for(int num : nums){
-            sum += num; 
-            count += map.getOrDefault(sum - k , 0);
-            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        // we maintain a running sum and see we have encountered currentSum - k
+        // we also maintain the frequency count of the encountered variables 
+        // if encountered the same sum twice we store sum -> frequency of occurence. 
+        HashMap<Integer, Integer> sumMap = new HashMap<>();
+        sumMap.put(0, 1);
+        int count = 0;
+        int currentSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            currentSum += nums[i];
+            count += sumMap.getOrDefault(currentSum - k, 0);
+            sumMap.put(currentSum, sumMap.getOrDefault(currentSum, 0) + 1);
         }
-        
-    return  count; 
-}
+
+        return count;
+    }
 }
