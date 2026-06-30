ea55ee12301ca3787271f0ad61a3d914d07dc740
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-06-30 13:24:00 -0700

    Time: 14 ms (62.06%), Space: 46.2 MB (78.69%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
@@ -1,36 +1,19 @@
 class Solution {
     public int numberOfSubstrings(String s) {
-        // native way check all substring from the begining character 
+        // Optimized approach using preposesing the last index of the characters
         int countSubstring = 0;
-        int firstA = -1;
-        int firstB = -1;
-        int firstC = -1;
+        int[] charCountArr = new int[3];
+        Arrays.fill(charCountArr, -1);
         for (int i = s.length() - 1; i >= 0; i--) {
             int lastChar = 0;
-            if (s.charAt(i) == 'a') {
-                firstA = i;
-                lastChar = firstB == -1 || firstC == -1 ? -1 : Math.max(firstB, firstC);
-            } else if (s.charAt(i) == 'b') {
-                firstB = i;
-                lastChar = firstA == -1 || firstC == -1 ? -1 : Math.max(firstA, firstC);
-            } else if (s.charAt(i) == 'c') {
-                firstC = i;
-                lastChar = firstB == -1 || firstA == -1 ? -1 : Math.max(firstB, firstA);
-            }
-
+            charCountArr[s.charAt(i) - 'a'] = i;
+            lastChar = charCountArr[0] == -1 || charCountArr[1] == -1 || charCountArr[2] == -1 ? -1
+                    : Math.max(Math.max(charCountArr[0], charCountArr[1]), charCountArr[2]);
             if (lastChar != -1) {
                 countSubstring += s.length() - lastChar;
             }
 
         }
         return countSubstring;
-    }
-
-    public boolean checkContains(int[] arr) {
-        for (int num : arr) {
-            if (num < 1)
-                return false;
-        }
-        return true;
     }
 }
