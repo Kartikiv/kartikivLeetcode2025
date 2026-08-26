71b10caff77c4fd77544c3b526c9d05484f22c4a
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-08-25 21:47:10 -0700

    Time: 0 ms (100%), Space: 44.1 MB (11.4%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,30 +1,33 @@
-
-import java.sql.RowId;
-
 class Solution {
+    // One method is the we can search the pivot and then find the element in left half or the right half 
+    // The second option is that we known that the everytime we find a mid we are effectively diving the array into 
+    // two part one part is always sorted and the other part is not sorted
+    // then we check if the element belongs to the sorted part or the unsorted part 
+    // then search continues till we find the element in this way we are optimize the traversal to a one pass rather
+    // than a two pass
     public int search(int[] nums, int target) {
-       int low = 0; 
-       int high = nums.length - 1; 
-       while (low <= high) {
-        int mid = low + (high - low) / 2; 
-        if(nums[mid] == target){
-            return mid; 
-        }
-        // check if left half is sorted and target is in the left half
-        else if(nums[mid] >= nums[low]){
-            if(target >= nums[low] && target < nums[mid]){
-                high = mid - 1;
-            }else{
-                low = mid + 1; 
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = right + (left - right) / 2;
+            if (nums[mid] == target) {
+                return mid;
             }
-            // right half is sorted
-        }else{
-            if(target <= nums[high] && target > nums[mid]){
-                low = mid + 1;
-            }else{
-                high = mid - 1;
+            // check if the left half is sorted
+            if (nums[mid] > nums[left]) {
+                if (target >= nums[left] && target <= nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+               if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
             }
         }
-       }
-    return  -1; }
+        return -1;
+    }
 }
