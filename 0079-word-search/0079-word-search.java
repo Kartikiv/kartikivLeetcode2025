25018f618f0ce806008977f1ca1bc9c27f633fa5
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-09-08 00:33:32 -0700

    Time: 274 ms (8.3%), Space: 46.8 MB (10.2%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -2,38 +2,39 @@ class Solution {
     boolean ans;
 
     public boolean exist(char[][] board, String word) {
-        this.ans = false;
+        boolean ans = false;
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                dfs(board, 0, i, j, word);
+               ans = ans || dfs(board, 0, i, j, word);
             }
         }
         return ans;
     }
 
-    public void dfs(char[][] board, int index, int i, int j, String word) {
+    public boolean dfs(char[][] board, int index, int i, int j, String word) {
         if (index == word.length()) {
-            ans = true;
-            return;
+            return true;
         }
         if (i < 0 || i >= board.length || j < 0 || j >= board[0].length) {
-            return;
+            return false;
         }
         if (word.charAt(index) != board[i][j]) {
-            return;
+            return false;
         }
         char temp = board[i][j];
         board[i][j] = '#';
+        boolean ans = false;
         int[][] directions = new int[][] { { -1, 0 }, { 1, 0 }, { 0, 1 }, { 0, -1 } };
         for (int[] direction : directions) {
             int newI = i + direction[0];
             int newJ = j + direction[1];
-            dfs(board, index + 1, newI, newJ, word);
+            ans = ans || dfs(board, index + 1, newI, newJ, word);
         }
         // back tracking and marking the steps as we visit here we can also maintain a visited bool array 
         // but the consequences are extra memory consumption. 
         board[i][j] = temp;
 
+        return ans;
     }
 
 }
