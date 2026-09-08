b1c8ca9ea06a263e8e105cac0e8eda31520197dd
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-09-08 00:29:22 -0700

    Time: 259 ms (9.11%), Space: 46.8 MB (10.2%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,35 +1,41 @@
 class Solution {
+    boolean ans;
+
     public boolean exist(char[][] board, String word) {
-       for(int i = 0; i < board.length; i++){
-        for(int j = 0; j < board[0].length; j++){ 
-           if (dfs(i, j, board, word, 0)){
-            return true;
-           }
+        this.ans = false;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                dfs(board, 0, i, j, word);
+            }
         }
-       } 
-    return false;
+        return ans;
     }
-    public boolean dfs(int i, int j,char[][] board, String word, int index){ 
-       
-        if(index == word.length()){ 
-            return true;
+
+    public void dfs(char[][] board, int index, int i, int j, String word) {
+        if (index == word.length()) {
+            ans = true;
+            return;
         }
-        
-         if(i < 0 || i >= board.length || j < 0 || j >= board[0].length){
-            return false;
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length) {
+            return;
         }
-        if(word.charAt(index) != board[i][j]){
-            return false;
+        if (word.charAt(index) != board[i][j]) {
+            return;
         }
         char temp = board[i][j];
         board[i][j] = '#';
-       boolean left =  dfs(i + 1, j,board, word, index + 1);
-       boolean right =  dfs(i - 1, j,board, word, index + 1);
-        boolean top = dfs(i, j + 1,board, word, index + 1);
-        boolean bottom = dfs(i, j - 1,board, word, index + 1);
-      
+        int[][] directions = new int[][] { { -1, 0 }, { 1, 0 }, { 0, 1 }, { 0, -1 } };
+        for (int[] direction : directions) {
+            int newI = i + direction[0];
+            int newJ = j + direction[1];
+
+            dfs(board, index + 1, newI, newJ, word);
+
+        }
+        // back tracking and marking the steps as we visit here we can also maintain a visited bool array 
+        // but the consequences are extra memory consumption. 
         board[i][j] = temp;
 
-    return top || bottom || left || right; 
     }
+
 }
