7d7760c881f797bb55241c4006990b26bdd5a996
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-09-14 03:38:42 -0700

    Time: 10 ms (71.59%), Space: 44.7 MB (43.75%) - LeetHub

--- 0631-design-excel-sum-formula/0631-design-excel-sum-formula.java
+++ 0631-design-excel-sum-formula/0631-design-excel-sum-formula.java
@@ -3,207 +3,137 @@ import java.util.*;
 class Cell {
     int value;
     boolean isFormula;
-
-    // Cells whose formulas depend on this cell
-    Set<Cell> children = new HashSet<>();
-
-    // Parent cell -> number of times it appears in formula
-    Map<Cell, Integer> formulaMap = new HashMap<>();
+    Set<Cell> children;
+    HashMap<Cell, Integer> formulaMap;
 
     public Cell(int value) {
-        this.value = value;
         this.isFormula = false;
+        this.value = value;
+        this.children = new HashSet<>();
     }
 
-    public int calculateValue() {
+    public Cell(HashMap<Cell, Integer> formulaMap) {
+        this.isFormula = true;
+        this.formulaMap = formulaMap;
+        this.value = calculateValue();
+        this.children = new HashSet<>();
+    }
+
+    public final int calculateValue() {
         if (!isFormula) {
             return value;
         }
-
-        int newValue = 0;
-
-        for (Map.Entry<Cell, Integer> entry : formulaMap.entrySet()) {
-            Cell parent = entry.getKey();
-            int weight = entry.getValue();
-
-            newValue += parent.value * weight;
+        value = 0;
+        for (Cell cell : formulaMap.keySet()) {
+            value += formulaMap.get(cell) * cell.value;
         }
-
-        value = newValue;
         return value;
     }
 }
 
 class Excel {
-
+    // every cell need to have it own topological ordering
+    // when we change a cell all the children need to updated as well
     Cell[][] matrix;
 
     public Excel(int height, char width) {
-
-        int columns = width - 'A' + 1;
-
-        matrix = new Cell[height][columns];
-
-        for (int row = 0; row < height; row++) {
-            for (int col = 0; col < columns; col++) {
-                matrix[row][col] = new Cell(0);
+        this.matrix = new Cell[height][width - 'A' + 1];
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width - 'A' + 1; j++) {
+                matrix[i][j] = new Cell(0);
             }
         }
+
     }
 
     public void set(int row, char column, int val) {
-
-        Cell cell = matrix[row - 1][column - 'A'];
-
-        // If this cell previously had a formula,
-        // disconnect it from its old parents.
-        removeOldFormula(cell);
-
-        // IMPORTANT: mutate existing object.
-        // Do NOT create a new Cell.
-        cell.isFormula = false;
-        cell.formulaMap.clear();
+        row -= 1;
+        Cell cell = matrix[row][column - 'A'];
+        // if cell is a value
+        if (cell.isFormula) {
+            // if a formula becomes a value then remove the dependency connections
+            for (Cell parent : cell.formulaMap.keySet()) {
+                parent.children.remove(cell);
+            }
+            cell.isFormula = false;
+            cell.formulaMap.clear();
+        }
         cell.value = val;
+        dfsTopogolicalUpdate(matrix[row][column - 'A']);
 
-        updateDependents(cell);
+    }
+
+    public void dfsTopogolicalUpdate(Cell cell) {
+        if (cell == null) {
+            return;
+        }
+        cell.calculateValue();
+        for (Cell child : cell.children) {
+            dfsTopogolicalUpdate(child);
+        }
     }
 
     public int get(int row, char column) {
-        return matrix[row - 1][column - 'A'].value;
+        row -= 1;
+        return matrix[row][column - 'A'].value;
     }
 
     public int sum(int row, char column, String[] numbers) {
-
-        Cell cell = matrix[row - 1][column - 'A'];
-
-        // Disconnect previous formula if one existed
-        removeOldFormula(cell);
-
-        Map<Cell, Integer> formula = parseFormula(numbers);
-
-        cell.isFormula = true;
-        cell.formulaMap = formula;
-
-        // Parent -> child dependency edges
-        for (Cell parent : formula.keySet()) {
-            parent.children.add(cell);
-        }
-
-        cell.calculateValue();
-
-        // Since this cell changed, update everything downstream.
-        updateDependents(cell);
-
-        return cell.value;
-    }
-
-    private void removeOldFormula(Cell cell) {
-
-        if (!cell.isFormula) {
-            return;
-        }
-
-        for (Cell parent : cell.formulaMap.keySet()) {
-            parent.children.remove(cell);
-        }
-
-        cell.formulaMap.clear();
-    }
-
-    private void updateDependents(Cell source) {
-
-        List<Cell> order = new ArrayList<>();
-        Set<Cell> visited = new HashSet<>();
-
-        dfs(source, visited, order);
-
-        // DFS postorder gives:
-        //
-        // descendant -> ... -> source
-        //
-        // Reverse to obtain:
-        //
-        // source -> ... -> descendant
-
-        Collections.reverse(order);
-
-        for (Cell cell : order) {
-            if (cell != source && cell.isFormula) {
-                cell.calculateValue();
+        row -= 1;
+        Cell c = matrix[row][column - 'A'];
+        if (c.isFormula) {
+            // if a formula becomes a value then remove the dependency connections
+            for (Cell parent : c.formulaMap.keySet()) {
+                parent.children.remove(c);
             }
         }
+        HashMap<Cell, Integer> formulaMap = parseFormula(numbers);
+        c.isFormula = true;
+        c.formulaMap = formulaMap;
+        c.calculateValue();
+        for (Cell cell : c.formulaMap.keySet()) {
+            cell.children.add(c); // added the created cell as dependency for the cell
+        }
+        dfsTopogolicalUpdate(c);
+        return matrix[row][column - 'A'].value;
     }
 
-    private void dfs(
-            Cell cell,
-            Set<Cell> visited,
-            List<Cell> order) {
+    private int[] parseCell(String s) {
+        int col = s.charAt(0) - 'A';
+        int row = Integer.parseInt(s.substring(1)) - 1;
 
-        if (!visited.add(cell)) {
-            return;
-        }
-
-        for (Cell child : cell.children) {
-            dfs(child, visited, order);
-        }
-
-        order.add(cell);
+        return new int[] { row, col };
     }
 
-    private Map<Cell, Integer> parseFormula(String[] numbers) {
-
-        Map<Cell, Integer> formula = new HashMap<>();
-
+    private HashMap<Cell, Integer> parseFormula(String[] numbers) {
+        HashMap<Cell, Integer> formulaMap = new HashMap<>();
         for (String s : numbers) {
-
             if (!s.contains(":")) {
-
-                Cell cell = getCell(s);
-
-                formula.put(
-                        cell,
-                        formula.getOrDefault(cell, 0) + 1
-                );
-
+                int [] pos = parseCell(s);
+                Cell cell = matrix[pos[0]][pos[1]];
+                formulaMap.put(cell, formulaMap.getOrDefault(cell, 0) + 1);
             } else {
-
                 String[] range = s.split(":");
-
-                int[] start = parsePosition(range[0]);
-                int[] end = parsePosition(range[1]);
-
-                for (int row = start[0]; row <= end[0]; row++) {
-
-                    for (int col = start[1]; col <= end[1]; col++) {
-
-                        Cell cell = matrix[row][col];
-
-                        formula.put(
-                                cell,
-                                formula.getOrDefault(cell, 0) + 1
-                        );
+                int [] start = parseCell(range[0]);
+                int [] end = parseCell(range[1]);
+                for (int i = start[0]; i <= end[0]; i++) {
+                    for (int j = start[1]; j <= end[1]; j++) {
+                        Cell cell = matrix[i][j];
+                        formulaMap.put(cell, formulaMap.getOrDefault(cell, 0) + 1);
                     }
                 }
+
             }
         }
-
-        return formula;
-    }
-
-    private Cell getCell(String position) {
-
-        int[] coordinates = parsePosition(position);
-
-        return matrix[coordinates[0]][coordinates[1]];
-    }
-
-    private int[] parsePosition(String position) {
-
-        int col = position.charAt(0) - 'A';
-
-        int row =
-                Integer.parseInt(position.substring(1)) - 1;
-
-        return new int[]{row, col};
+        return formulaMap;
     }
 }
+
+/**
+ * 
+ * Your Excel object will be instantiated and called as such:
+ * Excel obj = new Excel(height, width);
+ * obj.set(row,column,val);
+ * int param_2 = obj.get(row,column);
+ * int param_3 = obj.sum(row,column,numbers);
+ */
