9e6be6fa3b0feaa423dfd224ea4943becb30d9ab
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-09-16 05:36:40 -0700

    Time: 7 ms (14.6%), Space: 51.6 MB (80.23%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,30 +1,34 @@
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
 class Solution {
     public int numIslands(char[][] grid) {
-        int islands = 0;
+        int numIsland = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == '1') {
-                    dfs(grid, i, j);
-                    islands++;
+                    bfs(grid, i, j);
+                    numIsland++;
                 }
-
             }
         }
-        return islands;
+        return numIsland;
     }
 
-    public void dfs(char[][] grid, int i, int j) {
+    public void bfs(char[][] grid, int i, int j) {
+        Queue<Pair<Integer, Integer>> queue = new LinkedList<>();
+        queue.add(new Pair<>(i, j));
         grid[i][j] = '0';
-        int[][] directions = new int[][] { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
-        for (int[] direction : directions) {
-            int newI = i + direction[0];
-            int newJ = j + direction[1];
-            if (newI >= 0 && newI < grid.length && newJ >= 0 && newJ < grid[0].length
-                    && grid[newI][newJ] == '1') {
-
-                dfs(grid, newI, newJ);
+        while (!queue.isEmpty()) {
+            Pair<Integer, Integer> node = queue.poll();
+            int[][] directions = new int[][] { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
+            for (int[] direction : directions) {
+                int newI = node.getKey() + direction[0];
+                int newJ = node.getValue() + direction[1];
+                if (newI >= 0 && newI < grid.length && newJ >= 0 && newJ < grid[0].length && grid[newI][newJ] == '1') {
+                    queue.add(new Pair<>(newI, newJ));
+                    grid[newI][newJ] = '0';
+                }
             }
         }
     }
