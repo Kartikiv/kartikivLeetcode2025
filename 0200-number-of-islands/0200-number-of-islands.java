a9c1fa75d18dfdb912a22866f9f1e02c939972d2
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-09-16 05:42:45 -0700

    Time: 4 ms (46.17%), Space: 51.5 MB (82.55%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -2,6 +2,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 class Solution {
+    static int[][] directions = new int[][] { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
     public int numIslands(char[][] grid) {
         int numIsland = 0;
         for (int i = 0; i < grid.length; i++) {
@@ -16,12 +17,11 @@ class Solution {
     }
 
     public void bfs(char[][] grid, int i, int j) {
-        Queue<int[]> queue = new LinkedList<>();
+        Queue<int[]> queue =new ArrayDeque<>();
         queue.add(new int[]{i,j});
         grid[i][j] = '0';
         while (!queue.isEmpty()) {
             int[] node = queue.poll();
-            int[][] directions = new int[][] { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
             for (int[] direction : directions) {
                 int newI = node[0] + direction[0];
                 int newJ = node[1] + direction[1];
