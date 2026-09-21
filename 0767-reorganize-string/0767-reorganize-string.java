59b996f02668633a872a5523ed2da7c33d005ae9
Sai Kartik Ivaturi <122755518+Kartikiv@users.noreply.github.com>
2026-09-21 06:52:10 -0700

    Time: 3 ms (78.05%), Space: 43.2 MB (40.1%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,54 +1,54 @@
-
-import java.util.LinkedList;
 import java.util.PriorityQueue;
-import java.util.Queue;
 
-/*
- * @lc app=leetcode id=767 lang=java
- *
- * [767] Reorganize String
- */
-
-// @lc code=start
 class Solution {
     public String reorganizeString(String s) {
-        // Same task schedule with n == 1 
-        StringBuilder sb = new StringBuilder(); 
-        int [] freqMap = new int[26]; 
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> freqMap[b] - freqMap[a]);
-        for(char c : s.toCharArray()){ 
+
+        StringBuilder sb = new StringBuilder();
+
+        int[] freqMap = new int[26];
+        int mostFrequent = 0;
+
+        for (char c : s.toCharArray()) {
             freqMap[c - 'a']++;
+            mostFrequent = Math.max(
+                mostFrequent,
+                freqMap[c - 'a']
+            );
         }
-        for(int i = 0; i < 26 ; i++){ 
-            if(freqMap[i] > 0){
+
+        if (mostFrequent > Math.ceilDiv(s.length(), 2)) {
+            return "";
+        }
+
+        PriorityQueue<Integer> pq =
+            new PriorityQueue<>((a, b) -> freqMap[b] - freqMap[a]);
+
+        for (int i = 0; i < 26; i++) {
+            if (freqMap[i] > 0) {
                 pq.add(i);
             }
         }
-        int lastOccurred = -1;
-        while(!pq.isEmpty()){
-            Queue<Integer> coolDown = new LinkedList<>();
-        while(!pq.isEmpty()){ 
-            int character = pq.poll(); 
-            // check if we can add the character
-            if(lastOccurred != character){ 
-                sb.append((char)(character + 'a'));
-                lastOccurred = character;
-                freqMap[character]--;
-                if(freqMap[character] > 0){ 
-                    pq.add(character);
-                } 
-                break;      
-            }else{ 
-                coolDown.add(character);
-            }
-        }
-        pq.addAll(coolDown);
-        if(pq.size() == 1 && pq.peek() == lastOccurred){ 
-            return "";
-        }
-    }
-    return sb.toString(); 
-}           
-}
-// @lc code=end
 
+        int lastOccurred = -1;
+
+        while (!pq.isEmpty()) {
+
+            int character = pq.poll();
+
+            sb.append((char) (character + 'a'));
+            freqMap[character]--;
+
+            // Previous character has now cooled down
+            if (lastOccurred != -1 &&
+                freqMap[lastOccurred] > 0) {
+
+                pq.add(lastOccurred);
+            }
+
+            // Current character enters cooldown
+            lastOccurred = character;
+        }
+
+        return sb.toString();
+    }
+}
